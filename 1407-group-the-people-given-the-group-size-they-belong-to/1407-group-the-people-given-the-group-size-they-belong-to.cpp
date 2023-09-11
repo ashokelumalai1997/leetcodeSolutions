d8eb583158f0d82963b7e8f810b6ebad92872fe2
ashokelumalai1997 <31373343+ashokelumalai1997@users.noreply.github.com>
2023-09-11 08:14:39 +0530

    Time: 19 ms (14.73%), Space: 15.1 MB (5.29%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        map<int,vector<vector<int>>> countToGroups;
+        map<int,vector<int>> countToGroups;
+        vector<vector<int>> result;
         for(int i=0;i<groupSizes.size();i++)
         {
             if(countToGroups.find(groupSizes[i])==countToGroups.end())
             {
                 vector<int> temp = {i};
-                countToGroups[groupSizes[i]].push_back(temp);
+                countToGroups[groupSizes[i]]=temp;
             } else {
-                if((countToGroups[groupSizes[i]].end()-1)->size()<groupSizes[i]) {
-                    (countToGroups[groupSizes[i]].end()-1)->push_back(i);
+                if(countToGroups[groupSizes[i]].size()<groupSizes[i]) {
+                    countToGroups[groupSizes[i]].push_back(i);
                 }
                 else {
+                    result.push_back(countToGroups[groupSizes[i]]);
+                    countToGroups.erase(groupSizes[i]);
                     vector<int> temp = {i};
-                    countToGroups[groupSizes[i]].push_back(temp);
+                    countToGroups[groupSizes[i]]=(temp);
                 }
             }
         }
-        vector<vector<int>> result;
+        
         for(auto c:countToGroups){
-            for(auto ca:c.second)
-                result.push_back(ca);
+            
+                result.push_back(c.second);
         }
         return result;
     }
