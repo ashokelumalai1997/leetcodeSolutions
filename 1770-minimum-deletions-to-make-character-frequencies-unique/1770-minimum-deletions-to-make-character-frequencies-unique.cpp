df029eb2fe2b9013d688df1b2d933fbb1f4304c1
ashokelumalai1997 <31373343+ashokelumalai1997@users.noreply.github.com>
2023-09-12 19:45:01 +0530

    Time: 98 ms (27.47%), Space: 17.4 MB (37.86%) - LeetHub

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        unordered_map<char,int> charToFreqMap;
+        unordered_map<int,char> freqToCharMap;
+        for(auto c:s)
+        {
+            if(charToFreqMap.find(c)==charToFreqMap.end()) charToFreqMap[c]=1;
+            else charToFreqMap[c]++;
+        }
+        int res=0;
+        for(auto kv:charToFreqMap) {
+            if(freqToCharMap.find(kv.second)==freqToCharMap.end())
+            {
+                freqToCharMap[kv.second]=kv.first;
+                cout<<"if"<<kv.first<<kv.second;
+            } else {
+                cout<<"else";
+                int temp=kv.second;
+                while(temp>=0)
+                {
+                    if(freqToCharMap.find(temp)==freqToCharMap.end()) {
+                        if(temp) freqToCharMap[temp]=kv.first;
+                        res+=(kv.second-temp);
+                        cout<<res;
+                        break;
+                    }
+                    temp--;
+                }
+
+            }
+        }
+        return res;
+    }
+};
