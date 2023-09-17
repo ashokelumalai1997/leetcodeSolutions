0234b34bbee126694bee2b288938eab729bfd24d
ashokelumalai1997 <31373343+ashokelumalai1997@users.noreply.github.com>
2023-09-17 08:16:06 +0530

    Time: 749 ms (5.05%), Space: 44 MB (10.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,9 +6,8 @@ class Solution {
             nums.add(num);
         }
         for(int i2=0;i2<nums.size();i2++){
-            int num = nums.get(i2);
-            int i1 = nums.indexOf(target-num);
-            if(nums.contains(target-num)&&i1!=i2){
+            int i1 = nums.indexOf(target-nums.get(i2));
+            if(nums.contains(target-nums.get(i2))&&i1!=i2){
                 arr[0]=Integer.valueOf(i1);
                 arr[1]=Integer.valueOf(i2);
                 break;
