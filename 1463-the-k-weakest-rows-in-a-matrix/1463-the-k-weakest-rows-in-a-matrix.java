9b6a7b65edfacde6a193213cae57cd34d5bb4984
ashokelumalai1997 <31373343+ashokelumalai1997@users.noreply.github.com>
2023-09-18 07:15:56 +0530

    Time: 4 ms (15.72%), Space: 44.9 MB (17.24%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
         int[] result = new int[k];
         int[] temp = new int[mat.length];
-        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a,b)->{if(b.get(0)==a.get(0)) return a.get(1)-b.get(1);else return a.get(0)-b.get(0);});
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>(k,(a,b)->{if(b.get(0)==a.get(0)) return a.get(1)-b.get(1);else return a.get(0)-b.get(0);});
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat[0].length;j++){
                 temp[i]+=mat[i][j];
