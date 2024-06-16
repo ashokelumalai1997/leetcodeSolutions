2ef681eb2adcf62c380037dd112638d38c591675
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-06-16 18:48:14 +0530

    Time: 36 ms (100%), Space: 94.2 MB (100%) - LeetHub

---
+++ 3418-count-pairs-that-form-a-complete-day-ii/3418-count-pairs-that-form-a-complete-day-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long countCompleteDayPairs(int[] hours) {
+        Map<Integer, Integer> hoursFreqCounter = new HashMap<>();
+        long result = 0;
+        for(int hour : hours) {
+            int neededHour =  hour%24;
+            if(neededHour != 0) neededHour = 24 - neededHour;
+            if(hoursFreqCounter.containsKey(neededHour)) {
+                result += hoursFreqCounter.get(neededHour);
+            }
+            hoursFreqCounter.put(hour%24, hoursFreqCounter.getOrDefault(hour%24, 0) + 1);
+        }
+        return result;
+    }
+}
