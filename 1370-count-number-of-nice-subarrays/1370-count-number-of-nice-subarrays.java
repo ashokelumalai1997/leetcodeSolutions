8fe365863c709f63253e3e685e1b84b70f4483c7
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-06-22 11:32:30 +0530

    Time: 6 ms (97.91%), Space: 55.1 MB (36.41%) - LeetHub

---
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int consecutiveEvensAtPreviousWindowEnd = 0;
+        int left = 0;
+        int right = 0;
+        int evensAtStart = 0;
+        int currentWindowOddCount = 0;
+        int result = 0;
+        while(right < nums.length) {
+            currentWindowOddCount += nums[right]%2;
+            if(currentWindowOddCount == k) {
+                evensAtStart = 0;
+                while(currentWindowOddCount == k) {
+                    evensAtStart++;
+                    currentWindowOddCount -= nums[left]%2;
+                    left++;
+                }
+            }
+            result += evensAtStart;
+            right++;
+        }
+        return result;
+    }
+}
