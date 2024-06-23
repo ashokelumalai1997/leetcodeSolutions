c9ccfec4ec7e29d221fa203e0e715a1a2a026329
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-06-23 14:22:03 +0530

    Time: 17 ms (100%), Space: 60.8 MB (100%) - LeetHub

--- 3464-maximize-total-cost-of-alternating-subarrays/3464-maximize-total-cost-of-alternating-subarrays.java
+++ 3464-maximize-total-cost-of-alternating-subarrays/3464-maximize-total-cost-of-alternating-subarrays.java
@@ -1,20 +1,6 @@
 class Solution {
-    private long calculateMaxCost(int index, int multiplier, int[] nums, long[][] memo) {
-        if(index == nums.length) return 0;
-        if(memo[index][multiplier == -1 ? 0 : 1] != -1) return memo[index][multiplier == -1 ? 0 : 1];
-        long startingNew = nums[index] + calculateMaxCost(index + 1, -1, nums, memo);
-        long addingToExisting = nums[index]*multiplier + calculateMaxCost(index + 1, multiplier*-1, nums, memo);
-        return memo[index][multiplier == -1 ? 0 : 1] = Math.max(startingNew, addingToExisting);
-    }
+    
     public long maximumTotalCost(int[] nums) {
-        // long result = 0;
-        // long[][] memo = new long[nums.length][2];
-        // for(long[] m : memo) {
-        //     Arrays.fill(m, -1);
-        // }
-        // result = calculateMaxCost(0, 1, nums, memo);
-        // return result;
-
         long[][] dp = new long[nums.length + 1][2];
         for(int i = nums.length - 1; i >= 0; i --) {
             for(int j = 0; j < 2; j++ ) {
