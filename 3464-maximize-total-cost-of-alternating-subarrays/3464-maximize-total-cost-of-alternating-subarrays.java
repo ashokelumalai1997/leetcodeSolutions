85a786674f1666b926a452674302eb789af35e6a
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-06-23 14:26:39 +0530

    Time: 4 ms (100%), Space: 64.1 MB (100%) - LeetHub

--- 3464-maximize-total-cost-of-alternating-subarrays/3464-maximize-total-cost-of-alternating-subarrays.java
+++ 3464-maximize-total-cost-of-alternating-subarrays/3464-maximize-total-cost-of-alternating-subarrays.java
@@ -1,15 +1,18 @@
 class Solution {
     
     public long maximumTotalCost(int[] nums) {
-        long[][] dp = new long[nums.length + 1][2];
+        long[] dp = new long[2];
         for(int i = nums.length - 1; i >= 0; i --) {
+            long[] currentDp = new long[2];
             for(int j = 0; j < 2; j++ ) {
-                long startingNew = dp[i + 1][0] + nums[i];
-                long addingToExisting = dp[i + 1][j == 0? 1 : 0] + nums[i]*(j == 0 ? -1 : 1);
-                dp[i][j] = Math.max(startingNew, addingToExisting);
+                long startingNew = dp[0] + nums[i];
+                long addingToExisting = dp[j == 0? 1 : 0] + nums[i]*(j == 0 ? -1 : 1);
+                currentDp[j] = Math.max(startingNew, addingToExisting);
             }
+            dp[0] = currentDp[0];
+            dp[1] = currentDp[1];
         }
-        return dp[0][1];
+        return dp[1];
     }
 
 }
