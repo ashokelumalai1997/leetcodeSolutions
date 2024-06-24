0946df45e83f2cc7413be6b08a97884308ab8107
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-06-24 21:11:45 +0530

    Time: 37 ms (5.81%), Space: 55.8 MB (74.84%) - LeetHub

---
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int minKBitFlips(int[] nums, int k) {
+        Queue<Integer> oneQ = new LinkedList<>();
+        Queue<Integer> zeroQ = new LinkedList<>();
+        int n = nums.length;
+        int i = 0;
+        int result = 0;
+        Queue<Integer> temp = new LinkedList<>();
+        int tempK = 0;
+        while(i < n) {
+            tempK = oneQ.size() + zeroQ.size();
+            if(tempK == 0) {
+                while(i < n && nums[i] == 1) i++;
+            }
+            while(i < n && tempK < k) {
+                if(nums[i] == 0) zeroQ.offer(i);
+                else oneQ.offer(i);
+                i++;
+                tempK++;
+            }
+            if(i == n && oneQ.size() == 0 && zeroQ.size() == 0) return result;
+            // Queue<Integer> temp = new LinkedList<>(oneQ);
+            // oneQ.clear();
+            // oneQ.addAll(zeroQ);
+            // zeroQ.clear();
+            // zeroQ.addAll(temp);
+            temp = oneQ;
+            oneQ= zeroQ;
+            zeroQ = temp;
+            if(oneQ.size() == k) {
+                oneQ.clear();
+                result++;
+                continue;
+            }
+            while(!oneQ.isEmpty() && !zeroQ.isEmpty() && (oneQ.peek() < zeroQ.peek())) {
+                oneQ.poll();
+            }
+            result++;
+        }
+        if(!oneQ.isEmpty()) return -1;
+        if(!zeroQ.isEmpty()) {
+            if(zeroQ.size() == k) result++;
+            else return -1;
+        }
+        return result;
+    }
+}
