4453476f94250a727f87efa3e2ce77b8dee8b928
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-06-30 11:04:24 +0530

    Time: 91 ms (100%), Space: 55.2 MB (50%) - LeetHub

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.java
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.java
@@ -1,18 +1,32 @@
 class Solution {
     public int maximumLength(int[] nums, int k) {
-        int n=nums.length;
-        int ans=1;
-        int[][] dp = new int[n][k];
-        for(int[] d : dp) {
+        int n = nums.length;
+        Integer[][] dp = new Integer[n][k];
+        for(Integer[] d : dp) {
             Arrays.fill(d, 1);
         }
-        for(int i=0;i<n;++i){
-            for(int j=0;j<i;++j){
-                int x=(nums[i]+nums[j])%k;
-                dp[i][x]=dp[j][x]+1;
-                ans=Math.max(ans,dp[i][x]);
+
+        // Initialize base cases
+        for (int i = 0; i < n; i++) {
+            dp[i][nums[i] % k] = 1;
+        }
+
+        int res = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int mod = (nums[j] + nums[i]) % k;
+                if (dp[j][mod] != null) {
+                    dp[i][mod] = Math.max(dp[i][mod] == null ? 1 : dp[i][mod], dp[j][mod] + 1);
+                }
+            }
+            // Update the result with the longest subsequence found ending at index i
+            for (int mod = 0; mod < k; mod++) {
+                if (dp[i][mod] != null) {
+                    res = Math.max(res, dp[i][mod]);
+                }
             }
         }
-        return ans;
+
+        return res;
     }
 }
