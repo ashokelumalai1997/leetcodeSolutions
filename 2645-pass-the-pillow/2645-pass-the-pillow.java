33301a8832c1bd14ebfc82c8f5b8975548fb4e47
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-07-06 09:51:00 +0530

    Time: 0 ms (100%), Space: 40.3 MB (33.05%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.java
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -1,6 +1,7 @@
 class Solution {
     public int passThePillow(int n, int time) {
         time %= 2*(n - 1);
-        return (time <= n - 1) ? (time + 1) : (2*n - time - 1);
+        time++;
+        return (time <= n - 1) ? (time) : (2*n - time);
     }
 }
