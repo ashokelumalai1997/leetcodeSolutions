955cc0eeb2df5c89cd0cad8f5d3b176321fb3c28
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-07-07 10:05:24 +0530

    Time: 7 ms (100%), Space: 45.8 MB (100%) - LeetHub

--- 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.java
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.java
@@ -1,32 +1,26 @@
 class Solution {
     public List<String> validStrings(int n) {
-        List<String> result1 = new ArrayList<>();
+        List<String> result = new ArrayList<>();
         int ind = 1;
-        result1.add("0");
-        result1.add("1");
-        if(n == 1) return result1;
+        result.add("0");
+        result.add("1");
         String currentString = null;
-        List<String> re = new ArrayList<>();
+        // System.out.println(result.size());
         while(ind < n) {
-            List<String> result = new ArrayList<>();
-            for(int i = 0; i < result1.size();i++) {
-                currentString = result1.get(i);
-                
+            // System.out.println(result.size());
+            int cs = result.size();
+            for(int i = 0; i < cs;i++) {
+                // System.out.println(result.size());
+                currentString = result.get(i);
                 if(currentString.charAt(ind - 1) == '1') {
-                    result.add(currentString + "0");
+                    result.set(i, currentString + "0");
                     result.add(currentString + "1");
-                    if(ind == n -1) {
-                        re.add(currentString + "0");
-                        re.add(currentString + "1");
-                    }
                 } else {
-                    result.add(currentString + "1");
-                    if(ind == n -1) re.add(currentString + "1");
+                    result.set(i, currentString + "1");
                 }
             }
-            result1 = result;
             ind++;
         }
-        return re;
+        return result;
     }
 }
