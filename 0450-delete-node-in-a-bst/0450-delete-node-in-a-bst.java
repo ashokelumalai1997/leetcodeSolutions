34a4c15fdda718d42548135ca1c5c1da6c6c94e3
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-07-09 09:34:11 +0530

    Time: 0 ms (100%), Space: 46.2 MB (16.11%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int findMin(TreeNode root) {
+        // if(root == null) return -1;
+        if(root.left != null) return findMin(root.left);
+        return root.val;
+    }
+    private TreeNode deleteUtil(TreeNode root, int key) {
+        if(root == null) return null;
+        if(root.val == key) {
+            if(root.left == null && root.right == null) {
+                return null;
+            } else if(root.left == null) {
+                return root.right;
+            } else if(root.right == null) {
+                return root.left;
+            } else {
+                int minVal = findMin(root.right);
+                root.val = minVal;
+                root.right = deleteUtil(root.right, minVal);
+                return root;
+            }
+        }
+        root.left = deleteUtil(root.left, key);
+        root.right = deleteUtil(root.right, key);
+        return root;
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return deleteUtil(root, key);
+    }
+}
