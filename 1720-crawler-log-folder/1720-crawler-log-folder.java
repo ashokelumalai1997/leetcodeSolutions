f21412cc69ed6094d02d5c9a408684e017f0c168
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-07-10 09:10:12 +0530

    Time: 15 ms (44.79%), Space: 42.4 MB (8.45%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int count = 0;
+        for(int i = 0; i < logs.length; i++) {
+            if(logs[i].equals("../")) {
+                if(count != 0)count--;
+            }
+            else if(logs[i].equals("./") == false) {
+                System.out.println(logs[i]);
+                count++;
+            };
+        }
+        return count;
+    }
+}
