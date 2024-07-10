8f0f38fc7d808b1f8e53f64dfcea80c81f875e85
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-07-10 18:46:34 +0530

    Time: 533 ms (22.92%), Space: 53.2 MB (10.26%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,24 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
-        int k = 0;
-        int i = k+1;
-        int j = n-1;
-        
-        while(k < n-1) {
-            if(nums[k] > 0) break;
-            i = k+1;
-            j = n-1;
-            while(i < j) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == 0) {
-                    List<Integer> triplet = new ArrayList<Integer>();
-                    triplet.add(nums[i]);
-                    triplet.add(nums[k]);
-                    triplet.add(nums[j]);
-                    // Collections.sort(triplet);
-                    result.add(triplet);
-                    while(i < j && nums[i+1] == nums[i])i++;
-                    i++;
-                    while(j-1>=i+1 && nums[j] == nums[j-1])j--;
-                    j--;
-                } else if(sum < 0) {
-                    while(i < j && nums[i+1] == nums[i])i++;
-                    i++;
-                } else {
-                    while(j-1>=i+1 && nums[j] == nums[j-1])j--;
-                    j--;
+        Set<List<Integer>> res = new HashSet<>();
+        Set<Integer> dups = new HashSet<>();
+        // Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) if (dups.add(nums[i])) {
+            Map<Integer, Integer> seen = new HashMap<>();
+            for (int j = i + 1; j < nums.length; ++j) {
+                int complement = -nums[i] - nums[j];
+                if (seen.containsKey(complement) && seen.get(complement) == i) {
+                    List<Integer> triplet = Arrays.asList(
+                        nums[i],
+                        nums[j],
+                        complement
+                    );
+                    Collections.sort(triplet);
+                    res.add(triplet);
                 }
+                seen.put(nums[j], i);
             }
-            while(k+1 < n && nums[k+1] == nums[k]) k++;
-            k++;
         }
-        // return new ArrayList<>(result);
-        return result;
+        return new ArrayList(res);
     }
 }
