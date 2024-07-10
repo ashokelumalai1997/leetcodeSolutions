4aaae23127616c70e2455abb4aea683d4fe8c0b5
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-07-10 18:03:43 +0530

    Time: 59 ms (33.25%), Space: 52.8 MB (14.89%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -18,9 +18,9 @@ class Solution {
                     triplet.add(nums[k]);
                     // Collections.sort(triplet);
                     result.add(triplet);
-                    // while(i < j && nums[i+1] == nums[i])i++;
+                    while(i < j && nums[i+1] == nums[i])i++;
                     i++;
-                    // while(j-1>=i+1 && nums[j] == nums[j-1])j--;
+                    while(j-1>=i+1 && nums[j] == nums[j-1])j--;
                     j--;
                 } else if(sum < 0) {
                     i++;
