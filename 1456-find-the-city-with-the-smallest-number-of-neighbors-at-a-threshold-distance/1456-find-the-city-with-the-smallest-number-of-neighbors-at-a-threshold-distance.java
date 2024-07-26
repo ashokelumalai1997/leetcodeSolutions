6f0346dd159ea33b391615c8aa8fc8883ff25fa6
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-07-26 19:34:15 +0530

    Time: 103 ms (5.23%), Space: 45.3 MB (6.97%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -8,30 +8,39 @@ class Solution {
             int[] edges = edgesL[i];
             dist[edges[0]][edges[1]] = edges[2];
             dist[edges[1]][edges[0]] = edges[2];
+            dist[edges[0]][edges[0]] = 0;
+            dist[edges[1]][edges[1]] = 0;
         }
-        for(int i = 0; i < n; i++) {
-            dist[i][i] = 0; // Distance from a node to itself is 0
-        }
-        for(int k = 0; k < n; k++) {
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    if(dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE) {
-                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                    }
+        for(int i = 0; i < n; i ++){
+            for(int j = 0; j < n; j++) {
+                for(int k = 0; k < n; k++) {
+                    if(dist[j][i] == Integer.MAX_VALUE || dist[i][k] == Integer.MAX_VALUE) continue;
+                    dist[j][k] = Math.min(dist[j][k], dist[j][i] + dist[i][k]);
                 }
             }
         }
 
+        for(int i = 0; i < n; i ++){
+            for(int j = 0; j < n; j++) {
+                // for(int k = 0; k < n; k++) {
+                    // if(dist[i][j] == Integer.MAX_VALUE || dist[j][k] == Integer.MAX_VALUE) continue;
+                    // dist[i][k] = Math.min(dist[i][k], dist[i][j] + dist[j][k]);
+                    System.out.println("Distance from " + i + " to " + j + " " + dist[i][j]);
+                // }
+            }
+        }
+
+
         int[] counter = new int[n];
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i ++){
             for(int j = 0; j < n; j++) {
                 if(i != j && dist[i][j] <= distanceThreshold) counter[i]++;
             }
         }
-
         int result = 0;
         int resultCount = Integer.MAX_VALUE;
         for(int i = 0; i < n; i++) {
+            // System.out.print(i + " " + counter[i] + "\n");
             if(counter[i] <= resultCount) {
                 resultCount = counter[i];
                 result = i;
