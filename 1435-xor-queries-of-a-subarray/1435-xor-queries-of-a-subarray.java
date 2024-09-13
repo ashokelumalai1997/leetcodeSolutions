4c49ac8da0ec5a72aff9e370a03fa0115264d303
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-09-13 05:54:38 +0530

    Time: 2 ms (100%), Space: 57.6 MB (61.65%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -3,16 +3,11 @@ class Solution {
         int n = arr.length;
         int totalXOR = 0;
         int[] prefixXOR = new int[n];
-        int[] suffixXOR = new int[n];
         int XORTracker = 0;
-        int XORTrackerReverse = 0;
         for(int i = 0; i < n; i++) {
             totalXOR ^= arr[i];
             XORTracker ^= arr[i];
             prefixXOR[i] = XORTracker;
-
-            XORTrackerReverse ^= arr[n-1-i];
-            suffixXOR[n-1-i] = XORTrackerReverse;
         }
         int queriesLength = queries.length;
         int[] result = new int[queriesLength];
@@ -20,7 +15,7 @@ class Solution {
             int[] query = queries[i];
             result[i] = totalXOR;
             if(query[0] != 0) result[i] ^= prefixXOR[query[0]-1];
-            if(query[1] != n-1) result[i] ^= suffixXOR[query[1]+1];
+            result[i] ^= (totalXOR ^ prefixXOR[query[1]]);
         }
         return result;
 
