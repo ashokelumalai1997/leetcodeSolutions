9485187fd80e390b1495e58bfdf26f8ffc18bbf4
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-10-14 10:14:14 +0530

    Time: 122 ms (76.78%), Space: 63 MB (60.66%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
@@ -6,7 +6,7 @@ class Solution {
         while(k!=0 && !pq.isEmpty()) {
             int current = pq.poll();
             result += current;
-            pq.offer((int)Math.ceil(current/3.0));
+            pq.offer((int)Math.ceil(current/(double)3));
             k--;
         }
         return result;
