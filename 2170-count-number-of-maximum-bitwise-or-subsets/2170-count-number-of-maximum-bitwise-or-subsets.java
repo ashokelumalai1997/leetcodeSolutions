a8d44c55664baac3feffd185f685f18bec464d24
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-10-18 20:23:56 +0530

    Time: 77 ms (22.86%), Space: 62.2 MB (5.26%) - LeetHub

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,72 @@
+class Solution {
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOr = 0;
+        int n = nums.length;
+        for(int num : nums) {
+            maxOr |= num;
+        }
+
+        int[][] dp = new int[n][maxOr+1];
+        for(int i = 0; i < n; i++) {
+            dp[i][nums[i]]++;
+        }
+
+        for(int i = 0; i < n-1; i++) {
+            for(int j = 0; j <= maxOr; j++) {
+                if(dp[i][j] == 0) continue;
+                int nextOr = j | nums[i+1];
+                dp[i+1][nextOr] += dp[i][j];
+                dp[i+1][j] += dp[i][j];
+            }
+        }
+
+        return dp[n-1][maxOr];
+    }
+}
+
+
+//     3 1
+
+// 0.  0 0
+// 1.  0 0
+// 2.  0 0
+// 3.  1 2
+
+
+
+// dp[i][maxXor] denotes count of maxXor at ending at index i
+
+// go over ever maxXor at x-1, increment resultant xor count at i
+
+
+// maxXor starting at index i = Max of ( maxXor from i+1 ) | nums[i] and ( maxXor from i+1 ) 
+
+// maxXor at n-1 = nums[n-1]
+
+
+
+
+// 0000
+// 0001
+// 0010
+// 0011
+// 0100
+// ...
+
+
+
+// f(0)
+//     f(1,[0])
+//         f(2,[0,1])
+//             f(3)
+//             f(3)
+//         f(2)
+//             f(3)
+//             f(3)
+//     f(1, [])
+//         f(2, [1])
+//             f(3)
+//             f(3)
+//         f(2, [])
+//             f(3)
+//             f(3)
