0ffeb1497897e3a34177904865f11fbdbda5a7da
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-10-27 00:13:31 +0530

    Time: 0 ms (100%), Space: 40.4 MB (28.88%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        if(n <= 1) return 1;
+        dp[n] = 1;
+        dp[n-1] = 1;
+        for(int i = n-2; i >= 0 ; i--) {
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        return dp[0];
+    }
+}
