88dc59b22e0039a0268a24a35a91fdf062ed6530
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-10-27 14:19:24 +0530

    Time: 9 ms (14.63%), Space: 56.5 MB (10.64%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
@@ -1,28 +1,29 @@
-class Solution {
-    public boolean checkSquare(int i, int j, int k, int[][] mat) {
-        // for (int ii = i; ii <= i + k; ii++) {
-        //     for (int jj = j; jj <= j + k; jj++) {
-        //         if (mat[ii][jj] == 0) return false;
-        //     }
-        // }
-        for(int ii = i; ii <= i+k; ii++) if(mat[ii][j+k] == 0) return false;
-        for(int ii = j; ii <= j+k; ii++) if(mat[i+k][ii] == 0) return false;
+ class Solution {
+    public boolean checkCols(int i, int j, int k, int[][] mat) {
+        for(int ii = i; ii <= i+k; ii++) {
+            if(mat[ii][j+k] == 0) return false;
+        }
         return true;
     }
-    
+
+    public boolean checkRows(int i, int j, int k, int[][] mat) {
+        for(int jj = j; jj <= j+k; jj++) {
+            if(mat[i+k][jj] == 0) return false;
+        }
+        return true;
+    }
     public int countSquares(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         int total = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] != 1) continue;
-                
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] != 1) continue;
                 int mats = 0;
-                for (int k = 0; k + i < m && k + j < n; k++) {
-                    if (checkSquare(i, j, k, matrix)) {
-                        mats++;
+                for(int k = 0; k < Math.max(m,n); k++) {
+                    if(i+k < m && j+k < n && matrix[i+k][j+k] == 1) {
+                        if(checkRows(i, j, k, matrix) && checkCols(i, j, k, matrix)) mats++;
+                        else break;
                     } else {
                         break;
                     }
@@ -33,3 +34,11 @@ class Solution {
         return total;
     }
 }
+
+
+// for every m,n 300*300
+//     IF CURRENT CELL HAS 1
+//         for every right down ddiagonal cell n 300
+//             check all the sub mat cells (m*n) 300*300
+        
+
