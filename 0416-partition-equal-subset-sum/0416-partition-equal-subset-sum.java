ade2b5432aba624a63f55dea60c70ec46354e2b3
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-10-31 22:20:53 +0530

    Time: 108 ms (14.17%), Space: 57.3 MB (25.92%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,48 +1,55 @@
 class Solution {
 
-    private boolean canSplit(int[][] dp, int[] nums, int req, int index, int sumSoFar) {
-        if(index == nums.length) {
-            if(sumSoFar == req) return true;
-            return false;
-        }
+    private boolean canMakeTarget(int[][] dp, int ind, int target, int[] nums) {
+        if(target == 0) return true;
 
-        // if (sumSoFar > req) return false; // Early stopping
+        if(ind == nums.length) return false;
 
-        if(dp[index][sumSoFar] != -1) return dp[index][sumSoFar] == 1;
-        //canSplit(dp, [1,5,11,5], 11, , 0)
-        boolean take = canSplit(dp, nums, req, index+1, sumSoFar+nums[index]);
-        //canSplit(dp, [1,5,11,5], 11, 0, 0)
-        boolean notTake = canSplit(dp, nums, req, index+1, sumSoFar);
-        dp[index][sumSoFar] = (take || notTake) == true ? 1 : 0;
+        if(dp[ind][target] != -1) return dp[ind][target] == 1;
+
+        boolean take = false;
+        if(nums[ind] <= target) 
+            take = canMakeTarget(dp, ind+1, target-nums[ind], nums);
+        boolean notTake = canMakeTarget(dp, ind+1, target, nums);
+        dp[ind][target] = take || notTake ? 1 : 0;
         return take || notTake;
+
     }
     public boolean canPartition(int[] nums) {
         int total = 0;
         for(int num : nums) total += num;
-        if(total %2 != 0) return false;
-        int req = total/2;
-        int[][] dp = new int[nums.length][total+1];
-        for(int i = 0; i < nums.length; i++) Arrays.fill(dp[i], -1);
-        return canSplit(dp ,nums, req, 0, 0); //canSplit(dp, [1,5,11,5], 11, 0, 0)
+        if(total%2 != 0) return false;
+        int n = nums.length;
+
+        int[][] dp = new int[n][total/2+1];
+
+        for(int i = 0; i < n; i ++) Arrays.fill(dp[i], -1);
+
+        return canMakeTarget(dp, 0, total/2, nums);
     }
 }
 
 
+// find total
 
-//canSplit(dp, [1,5,11,5], 11, 0, 0)
+// // total/2 is the target . Return if you can find the target
 
-// f(0,0)
-//     f(1,1)
-//         f(5,2)
-//             f(16,3)
-//             f(5,3)
-//         f(1,2)
-//             f(12, 3)
-//             f(1,3)
-//     f(1,0)
-//         f(2,2)
-//             f(14,3)
-//             f(2,3)
-//         f(2,2)
-//             f()
 
+// // f(i, sum) denotes if sum can be formed from ith index
+// //     if(sum == 0) return true
+// //     if(i == n) return false
+// //     a f(i+1, sum)
+// //     b f(i+1, sum-nums[i])
+// //     return a || b
+
+
+// // 1 5 11 5
+
+// // f(0, 11)
+// //     f(1, 11)
+// //         f(2, 11)
+// //             f(3, 11)
+// //             f(3, 0)
+// //         f(2, 6)
+        
+// //     f(1, 10)
