ed4f5022211ef79db8452dbbecc60047fdf28a21
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-10-31 20:18:22 +0530

    Time: 93 ms (95.88%), Space: 79.6 MB (85.61%) - LeetHub

---
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.java
@@ -0,0 +1,69 @@
+class Task  implements Comparable<Task> {
+
+    int start;
+    int processingTime;
+    int index;
+
+    public Task(int s, int p, int i) {
+        this.start = s;
+        this.processingTime = p;
+        this.index = i;
+    }
+
+    public int compareTo(Task o) {
+        if(processingTime == o.processingTime) return index - o.index;
+        return processingTime - o.processingTime;
+    }
+}
+
+class Solution {
+    public int[] getOrder(int[][] tasks) {
+        List<Task> taskList = new ArrayList<>();
+        int n = tasks.length;
+        for(int i = 0; i < n; i++) {
+            taskList.add(new Task(tasks[i][0], tasks[i][1], i));
+        }
+        Collections.sort(taskList, (a,b) -> a.start - b.start);
+
+        int current = taskList.get(0).start;
+
+        int taskTracker = 0;
+
+        PriorityQueue<Task> pq = new PriorityQueue<>();
+
+        while(taskTracker < n && taskList.get(taskTracker).start == current) {
+            pq.offer(taskList.get(taskTracker));
+            taskTracker++;
+        }
+
+        
+
+        int[] taskOrder = new int[n];
+
+        int taskOrderTracker = 0;
+
+        while(!pq.isEmpty()) {
+            Task currentTask = pq.poll();
+            taskOrder[taskOrderTracker] = currentTask.index;
+            taskOrderTracker++;
+            current = current + currentTask.processingTime;
+            
+            for(; 
+                        taskTracker < n && taskList.get(taskTracker).start <= current; taskTracker++) {
+                pq.offer(taskList.get(taskTracker));
+            }
+            if(pq.isEmpty() && taskTracker < n) {
+                current = taskList.get(taskTracker).start;
+                while(taskTracker < n && taskList.get(taskTracker).start <= current) {
+                    pq.offer(taskList.get(taskTracker));
+                    taskTracker++;
+                }
+            }
+            // taskTracker = i;
+        }
+
+        return taskOrder;
+
+
+    }
+}
