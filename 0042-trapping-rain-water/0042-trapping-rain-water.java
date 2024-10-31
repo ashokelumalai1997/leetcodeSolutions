e2eb48ce35cefc039908f189d84b2dcb74e0e811
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-10-31 20:29:35 +0530

    Time: 1 ms (66.65%), Space: 45.7 MB (91.74%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,20 +3,24 @@ class Solution {
         int n = height.length;
         int[] leftMax = new int[n];
         int[] rightMax = new int[n];
-        int lmax = 0;
-        int rmax = 0;
-        for(int i = 0; i < n; i++) {
-            leftMax[i] = lmax;
-            lmax = Math.max(height[i], lmax);
-            rightMax[n-1-i] = rmax;
-            rmax = Math.max(height[n-1-i], rmax);
+
+        leftMax[0] = 0;
+        for(int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i-1], height[i-1]);
         }
-        int result = 0;
-        int ans = 0;
-        for(int i = 0; i < n; i++) {
-            ans = Math.min(leftMax[i], rightMax[i]) - height[i];
-            result += ans >= 0 ? ans : 0;
+
+        rightMax[n-1] = 0;
+        for(int i = n-2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i+1], height[i+1]);
         }
-        return result;
+
+        int water = 0;
+
+        for(int i = 0; i < n; i++) {
+            int possibleHeight  = Math.min(leftMax[i], rightMax[i]);
+            if(possibleHeight > height[i]) water += (possibleHeight - height[i]);
+        }
+
+        return water;
     }
 }
