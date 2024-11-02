43ed56763e46352e6d0d3863755b7f91ee5562e0
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-11-03 00:36:25 +0530

    Time: 2599 ms (5.02%), Space: 54 MB (5.24%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,61 +1,23 @@
-// sort the nums
-// for every index
-//     if index == prevind continue
-//     target becomes -nums[index]
-//     left = next index
-//     right = end
-//     while(left < right) {
-//         if(left + right == 0) add them to the q. move dups
-//         if left+right > 0 right--
-//         else left--
-//     }
-// [-4, -1, -1, 0, 1, 2]
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        List<List<Integer>> res = new ArrayList<>();
-        for(int i = 0; i < n; i ++) {
-            if(i-1 >=0 && nums[i-1] == nums[i]) continue;
-            int target = -1*nums[i];
-            int left = i+1;
-            int right = n-1;
-            while(left < right) {
-                // if(left-1 != i && nums[left] == nums[left-1]) {
-                //     left++;
-                //     continue;
-                // }
-                if(nums[left] + nums[right] == target) {
-                    List<Integer> newEntry = new ArrayList<>();
-                    newEntry.add(nums[i]);
-                    newEntry.add(nums[left]);
-                    newEntry.add(nums[right]);
-                    res.add(newEntry);
-                    // while(left+1 < n && nums[left] == nums[left+1]) left++;                  
-                    
-                    left++;
-                    while(left < n && nums[left - 1] == nums[left]) left++;
-                } else if(nums[left] + nums[right] > target) {
-                    right--;
-                } else {
-                    left++;
+        Set<List<Integer>> res = new HashSet<>();
+        Set<Integer> dups = new HashSet<>();
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i)  {
+            for (int j = i + 1; j < nums.length; ++j) {
+                int complement = -nums[i] - nums[j];
+                if (seen.containsKey(complement) && seen.get(complement) == i) {
+                    List<Integer> triplet = Arrays.asList(
+                        nums[i],
+                        nums[j],
+                        complement
+                    );
+                    Collections.sort(triplet);
+                    res.add(triplet);
                 }
-               
+                seen.put(nums[j], i);
             }
-            // while()
         }
-        return res;
+        return new ArrayList(res);
     }
 }
-
-
-// for every index
-//  use a hashmap
-//  target becomes target - nums[index]
-//  from next index
-//    check if current - new target is in hm
-//    yes : add nums[i], current-new target, current to the list
-
-
-
-
