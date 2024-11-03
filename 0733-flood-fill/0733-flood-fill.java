067ef9db63718bdff3ddbb059a5eb4e5119fbf70
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-11-03 14:03:42 +0530

    Time: 1 ms (46.76%), Space: 45 MB (20.57%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,32 +1,34 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        Stack<Pair<Integer,Integer>> s = new Stack<>();
-        Pair<Integer,Integer> source = new Pair<>(sr,sc);
-        s.push(source);
-        int rows = image.length;
-        int cols = image[0].length;
-        int sourceColor = image[sr][sc];
-        while(!s.isEmpty()){
-            Pair<Integer,Integer> temp = s.peek();
-            
-            int r = temp.getKey();
-            int c = temp.getValue();
-            s.pop();
-            if(image[r][c]==color) continue;
-            if(r-1>=0&&image[r-1][c]==sourceColor){
-                s.push(new Pair<Integer,Integer>(r-1,c));
+        if(image[sr][sc] == color) return image;
+        int originalColor = image[sr][sc];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sr, sc});
+        int[][] directions = new int[][]{
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+        int m = image.length;
+        int n = image[0].length;
+        image[sr][sc] = color;
+        while(!q.isEmpty()) {
+            int[] node = q.poll();
+            int r = node[0];
+            int c = node[1];
+            // image[r][c] = color;
+            for(int[] dir : directions) {
+                if(r + dir[0] < m && r+ dir[0] >= 0
+                    && c +dir[1] < n && c + dir[1] >= 0) {
+                if(image[r+dir[0]][c+dir[1]] == originalColor) {
+                    image[r+dir[0]][c+dir[1]] = color;
+                    q.offer(new int[]{r+dir[0], c+dir[1]});
+                }
+                    }
             }
-            if(r+1<rows&&image[r+1][c]==sourceColor){
-                s.push(new Pair<Integer,Integer>(r+1,c));
-            }
-            if(c-1>=0&&image[r][c-1]==sourceColor){
-                s.push(new Pair<Integer,Integer>(r,c-1));
-            }
-            if(c+1<cols&&image[r][c+1]==sourceColor){
-                s.push(new Pair<Integer,Integer>(r,c+1));
-            }
-            image[r][c]=color;
         }
+
         return image;
     }
 }
