d151c26f1a75f7229fd46c223a9e11f4cd108966
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-11-04 20:18:42 +0530

    Time: 109 ms (26.44%), Space: 132.6 MB (10.64%) - LeetHub

---
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
@@ -0,0 +1,57 @@
+class Solution {
+
+    int time;
+
+    private void fillAllCriticalEdges(int node, int[] low, int[] disc, 
+        boolean[] vis, int parent, List<List<Integer>> criticalEdges, 
+        List<List<Integer>> adjList) {
+
+            vis[node] = true;
+            low[node]  =time;
+            disc[node] = time;
+            time++;
+
+            for(int child : adjList.get(node)) {
+                if(!vis[child]) {
+                    fillAllCriticalEdges(child, low, disc, vis, 
+                        node, criticalEdges, adjList);
+                    low[node] = Math.min(low[child], low[node]);
+                    if(low[child] > disc[node]) {
+                        List<Integer> edge = new ArrayList<>();
+                        edge.add(node);
+                        edge.add(child);
+                        criticalEdges.add(edge);
+                    }
+                } else {
+                    if(parent != child)
+                    low[node] = Math.min(low[node], disc[child]);
+                }
+            }
+
+    }
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i = 0 ; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for(List<Integer> edge : connections) {
+            int u = edge.get(0);
+            int v = edge.get(1);
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
+        }
+
+        List<List<Integer>> criticalEdges = new ArrayList<>();
+
+        boolean[] vis = new boolean[n];
+        int[] low = new int[n];
+        int[] disc = new int[n];
+
+        time = 1;
+
+        fillAllCriticalEdges(0, low, disc, vis, -1, criticalEdges, adjList);
+
+        return criticalEdges;
+    }
+}
