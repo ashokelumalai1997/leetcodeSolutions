3811045dcd035afd6ec50bc2e34be34b310d17d6
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-11-05 09:01:40 +0530

    Time: 3 ms (97.78%), Space: 44.7 MB (51.56%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -3,8 +3,7 @@ class Solution {
         int n = s.length();
         int changes = 0;
         for(int i = 0; i <n; i += 2) {
-            if(s.charAt(i) == s.charAt(i+1)) continue;
-            changes++;
+            if(s.charAt(i) != s.charAt(i+1)) changes++;
         }
         return changes;
     }
