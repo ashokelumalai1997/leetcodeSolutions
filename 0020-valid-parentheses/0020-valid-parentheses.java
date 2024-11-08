85d11dbe3076a0b98a6344547552011897fed116
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-11-08 14:37:22 +0530

    Time: 2 ms (97.17%), Space: 41.7 MB (24.86%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,36 @@
 class Solution {
+
+    private Character getMatch(Character c) {
+        if(c == '}') return '{';
+        if(c == ')') return '(';
+        return '[';
+    }
+    
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<Character>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='{'||s.charAt(i)=='['||s.charAt(i)=='('){
-                st.add(s.charAt(i));
-                continue;
-            }
-            if(st.isEmpty()) return false;
-            if( s.charAt(i)=='}'&&st.peek()!='{' || 
-                s.charAt(i)==']'&&st.peek()!='[' ||
-                s.charAt(i)==')'&&st.peek()!='(' ) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            if(s.charAt(i) == '{'
+                || s.charAt(i) == '['
+                || s.charAt(i) == '(') {
+            st.push(s.charAt(i));
+            } else {
+                if(!st.isEmpty() 
+                    && st.peek() == getMatch(s.charAt(i))) {
+                    st.pop();
+                } else {
                     return false;
                 }
-            st.pop();
+            }
         }
-        if(st.isEmpty()){
-            return true;
-        }
-        return false;
+
+        return st.isEmpty();
     }
 }
+
+// { [ ( -> push into stack
+
+// } ) ] -> top should match and not empty
+
+// finally stack should be empty
+
