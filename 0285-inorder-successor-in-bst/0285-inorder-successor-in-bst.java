8552615faece1822d8d218d204c9e578a12a73b2
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-11-08 14:30:43 +0530

    Time: 2 ms (100%), Space: 45.7 MB (51.6%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.java
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.java
@@ -13,28 +13,28 @@ class Solution {
         if(root.left == null) return root;
         return getMin(root.left);
     }
-    public TreeNode ios(TreeNode root, TreeNode p, Stack<TreeNode> s) {
+    public TreeNode ios(TreeNode root, TreeNode p, TreeNode parent) {
         if(root == null) return null;
 
         if(root.val == p.val) {
             if(root.right != null) return getMin(root.right);
-            if(s.size() != 0) return s.peek();
-            return null;
+            // if(s.size() != 0) return s.peek();
+            return parent;
         }
 
-        s.push(root);
-        TreeNode l = ios(root.left , p, s);
+        // s.push(root);
+        TreeNode l = ios(root.left , p, root);
 
         if(l != null) return l;
-        s.pop();
-        TreeNode r = ios(root.right, p, s);
+        // s.pop();
+        TreeNode r = ios(root.right, p, parent);
 
         return r;
 
     }
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         // if(root == null) return null;
-        return ios(root, p, new Stack<>());
+        return ios(root, p, null);
 
     }
 }
