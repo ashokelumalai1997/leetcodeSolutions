58ab31e30fb6fa484a01fd0c88eaf0749324b1bc
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-11-10 14:33:31 +0530

    Time: 6 ms (74.54%), Space: 45.5 MB (37.26%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,54 +1,57 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        //Create a adj list of vertex to child
-        //Create a a hashMap of indegree
-        //Push all the sources to a Queue
-        //When Queue is not empty, pick source one by one
-        //Push the value to a list
-        //Decrease indegree of all it's childs
-        //If indegree is zero for any child push it to Queue
-        //Remove the current node from the queue
-        List<Integer>[] adjList = new ArrayList[numCourses];
-        //Arrays.fill(adjList,new ArrayList());
-        for(int i=0;i<numCourses;i++){
-            adjList[i]= new ArrayList<>();
+        int[] inDegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int c = 0; c < numCourses; c++) {
+            adj.add(new ArrayList<>());
         }
-        for(int i=0;i<numCourses;i++){
-            System.out.println(adjList[i].toString());
-        }
-        HashMap<Integer,Integer> inDegree = new HashMap<>();
-        for(int i=0;i<numCourses;i++){
-            inDegree.put(i,0);
-        }
-        System.out.println(inDegree.toString());
-        for(int i=0;i<prerequisites.length;i++){
-            adjList[prerequisites[i][0]].add(prerequisites[i][1]);
-            inDegree.put(prerequisites[i][1],inDegree.get(prerequisites[i][1])+1);
-        }
-        Queue<Integer> sourceQ = new LinkedList<>();
-        for(Map.Entry<Integer,Integer> entry : inDegree.entrySet()){
-            
-            System.out.println(entry.toString());
-            int val=entry.getValue();
-            if(val==0){
-                sourceQ.offer(entry.getKey());
-            }
-        }
-        List<Integer> sortedOrder = new ArrayList<>();
-        while(!sourceQ.isEmpty()){
-            int currentSource = sourceQ.peek();
-            sourceQ.poll();
-            sortedOrder.add(currentSource);
-            for(Integer node : adjList[currentSource]){
-                inDegree.put(node,inDegree.get(node)-1);
-                if(inDegree.get(node)==0)
-                    sourceQ.offer(node);
-            }
-        }
-        //System.out.println(sortedOrder.toString());
-        if(sortedOrder.size()!=numCourses)
-            return false;
-        return true;
+        for(int[] pr : prerequisites) {
+            inDegree[pr[1]]++;
+            adj.get(pr[0]).add(pr[1]);
 
+        }
+
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 0; i < numCourses; i++) {
+            if( inDegree[i] == 0 ) {
+                q.offer(i);
+            }
+        }
+
+        int courses = 0;
+
+        while(!q.isEmpty()) {
+            int currentCourse = q.poll();
+
+            courses++;
+
+            for(int child : adj.get(currentCourse)) {
+                inDegree[child]--;
+                if(inDegree[child] == 0) {
+                    q.offer(child);
+                }
+            }
+        }
+
+        return courses == numCourses;
     }
 }
+
+//     1       2       3       4       5       6
+//         7       8       9
+//             10      11
+//                 12
+
+// push courses without prereq into queue
+
+
+// poll from q one by one
+//     each poll is a course
+
+//     deduct prereqs of child courses
+//     if(child course becomes zero prereq) push it to q
+
+
+// numCourses must have been polled
