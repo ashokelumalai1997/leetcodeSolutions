318bbdecb951c243fd3027ca48d117d924941c5f
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-12-03 09:02:01 +0530

    Time: 0 ms (100%), Space: 45.8 MB (71.24%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,16 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] range = new int[]{-1, -1};
+        int firstOccurrence = this.findBound(nums, target, true);
 
-        int n = nums.length;
+        if (firstOccurrence == -1) {
+            return new int[] { -1, -1 };
+        }
 
-        for(int i = 0; i < n; i ++) {
-            if(nums[i] == target) {
-                if(range[0] == -1) range[0] = i;
-                range[1] = i;
+        int lastOccurrence = this.findBound(nums, target, false);
+
+        return new int[] { firstOccurrence, lastOccurrence };
+    }
+
+    private int findBound(int[] nums, int target, boolean isFirst) {
+        int N = nums.length;
+        int begin = 0, end = N - 1;
+        int res = -1;
+
+        while (begin <= end) {
+            int mid = (begin + end) / 2;
+
+            if (nums[mid] == target) {
+                if (isFirst) {
+                    // This means we found our lower bound.
+                    res = mid;
+                    end = mid - 1;
+                } else {
+                    res = mid;
+                    begin = mid + 1;
+                }
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                begin = mid + 1;
             }
         }
 
-        return range;
+        return res;
     }
 }
