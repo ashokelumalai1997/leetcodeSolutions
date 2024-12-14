3738db37a6847d9b1dc20499e893a85611beb00f
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-12-14 12:09:58 +0530

    Time: 5 ms (33.73%), Space: 52.1 MB (18.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,21 +1,80 @@
 class Solution {
 
-    private void dfs(char[][] grid, int r, int c, int m, int n) {
+    // private void dfs(char[][] grid, int r, int c, int m, int n) {
+    //     grid[r][c] = '0';
+
+
+    //     int[][] pos = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    //     for(int[] pp : pos) {
+
+    //         int p = pp[0];
+    //         int p1 = pp[1];
+    //         // for(int p1 : pos) {
+    //             if(r+p >= 0 && r+p < m && c + p1 >= 0 && c+p1 < n && grid[r+p][c+p1] == '1') {
+    //                 dfs(grid, r+p, c+p1, m,n);
+    //             }
+    //         // }
+    //     }
+    // }
+
+    static class Position {
+
+        int row;
+
+        int col;
+
+
+        public Position(int r, int c) {
+            this.row = r;
+            this.col = c;
+        }
+
+    }
+
+    private void bfs(char[][] grid, int r, int c, int m, int n) {
+
+
+        Queue<Position> q = new LinkedList<>();
+
+
+        int[][] positions = new int[][]{
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+
+
+        q.offer(new Position(r, c));
+
         grid[r][c] = '0';
 
 
-        int[][] pos = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-        for(int[] pp : pos) {
 
-            int p = pp[0];
-            int p1 = pp[1];
-            // for(int p1 : pos) {
-                if(r+p >= 0 && r+p < m && c + p1 >= 0 && c+p1 < n && grid[r+p][c+p1] == '1') {
-                    dfs(grid, r+p, c+p1, m,n);
+        while(!q.isEmpty()) {
+
+            Position qe = q.poll();
+            int row = qe.row;
+            int col = qe.col;
+
+
+            
+
+            for(int[] pos : positions) {
+                if(row + pos[0] >= 0 
+                    && row + pos[0] < m
+                    && col + pos[1] >= 0
+                    && col + pos[1] < n
+                    && grid[row + pos[0]][col + pos[1]] == '1') {
+                    grid[row + pos[0]][col + pos[1]] = '0';
+                    q.offer(new Position(row + pos[0], col + pos[1]));
                 }
-            // }
+            }
         }
+
+
     }
     public int numIslands(char[][] grid) {
         int m = grid.length;
@@ -26,7 +85,7 @@ class Solution {
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(grid[i][j] == '1') {
-                    dfs(grid, i, j, m, n);
+                    bfs(grid, i, j, m, n);
                     count++;
                 }
             }
