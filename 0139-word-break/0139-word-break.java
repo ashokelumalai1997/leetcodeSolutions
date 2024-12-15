62998c08b0659d87594e370c551b38618a468eb0
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-12-15 22:26:59 +0530

    Time: 6 ms (60.09%), Space: 44.8 MB (15.19%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,83 +1,33 @@
 class Solution {
-    static class Trie {
-        static class TrieNode {
-            TrieNode[] child;
-            boolean isEndOfWord;
-            public TrieNode() {
-                child = new TrieNode[26];
-                for(int i = 0; i < 26; i++) {
-                    child[i] = null;
-                }
-                isEndOfWord = false;
+
+    private boolean breakable(int[] memo, int ind, String s, Set<String> set) {
+        if(ind == s.length()) return true;
+
+        if(memo[ind] != -1) return memo[ind] == 1;
+
+        boolean result = false;
+
+        for(int i = ind + 1; i <= s.length(); i++) {
+            if(set.contains(s.substring(ind, i))) {
+                result = result || breakable(memo, i, s, set);
             }
         }
 
-        private TrieNode head;
+        memo[ind] = (result ? 1 : 0);
 
-        public Trie() {
-            head = new TrieNode();
-        }
-
-        public void addWord(String word) {
-                char[] wordChars = word.toCharArray();
-
-                TrieNode temp = head;
-
-                for(char ch : wordChars) {
-                    if(temp.child[ch - 'a'] == null) {
-                        temp.child[ch - 'a'] = new TrieNode();
-                    }
-                    temp = temp.child[ch - 'a'];
-                }
-
-                temp.isEndOfWord = true;
-        }
-
-        public TrieNode getRef() {
-            return head;
-        }
+        return result;
     }
-
-    private boolean isPossible(int[] breakMemo, int i, String s, Trie trie, Trie.TrieNode trieNode) {
-        
-
-        if(trieNode.child[s.charAt(i) - 'a'] == null) return false;
-
-        boolean breakoption = false;
-
-        if(i+1 == s.length()) {
-            return trieNode.child[s.charAt(i) - 'a'].isEndOfWord;
-        }
-
-        if(trieNode.child[s.charAt(i) - 'a'].isEndOfWord) {
-            if(breakMemo[i+1] != -1) breakoption = breakMemo[i+1] != 0;
-            else {
-                
-                breakoption = isPossible(breakMemo, i+1, s, trie, trie.getRef());
-                breakMemo[i+1] = breakoption ? 1 : 0;
-            };
-        }
-
-        boolean contOption = false;
-
-        contOption = isPossible(breakMemo, i+1, s, trie, trieNode.child[s.charAt(i) - 'a']);
-
-
-        return breakoption || contOption;
-    }
-
     public boolean wordBreak(String s, List<String> wordDict) {
-        Trie trie = new Trie();
+        Set<String> set = new HashSet<>();
 
+        int[] memo = new int[s.length()];
+
+        Arrays.fill(memo, -1);
         for(String word : wordDict) {
-            trie.addWord(word);
+            set.add(word);
         }
 
-        int[] breakMemo = new int[s.length()];
 
-        Arrays.fill(breakMemo, -1);
-
-        return isPossible(breakMemo, 0, s, trie, trie.getRef());
-
+        return breakable(memo, 0, s, set);
     }
 }
