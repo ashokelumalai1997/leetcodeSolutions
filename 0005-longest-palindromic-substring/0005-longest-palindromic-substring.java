0e7b74429babfad403c780f7de28be2244a61b5d
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-12-15 21:40:50 +0530

    Time: 88 ms (44.66%), Space: 55.5 MB (5.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,32 +1,56 @@
 class Solution {
-    private boolean isPal(int start, int end, String s, int[][] memo) {
-        if(start >= end) return true;
-
-        if(memo[start][end] != -1) return memo[start][end] == 1;
-
-        boolean res = s.charAt(start) == s.charAt(end) 
-                            && isPal(start+1, end-1, s, memo);
-        
-        memo[start][end] = res ? 1: 0;
-
-        return memo[start][end] == 1;
-    }
     public String longestPalindrome(String s) {
         int n = s.length();
+        int[][] dp = new int[n][n];
 
-        int[][] memo = new int[n][n];
+        String res = s.substring(0,1);
+
 
         for(int i = 0; i < n; i++) {
-            Arrays.fill(memo[i], -1);
+            dp[i][i] = 1;
+            if(i != 0) {
+                dp[i-1][i] = (s.charAt(i) == s.charAt(i-1) ? 1 : 0);
+                if(dp[i-1][i] == 1) res = s.substring(i-1,i+1);
+                // res = 2;
+            }
         }
 
-        for(int i = n-1; i >= 0; i --) {
-            for(int j = 0; j < n; j++) {
-                if(j+i < n && isPal(j, j+i, s, memo)) {
-                    return s.substring(j, j+i+1);
+        
+
+        for(int i = 2; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(dp[j-i+1][j-1] == 1) {
+                    dp[j-i][j] = (s.charAt(j-i) == s.charAt(j) ? 1 : 0);
+                    if(dp[j-i][j] == 1 && i+1 > res.length())
+                        res = s.substring(j-i,j+1);
                 }
             }
         }
-        return null;
+
+        return res;
+
     }
 }
+
+
+
+
+//     1   2   3   4   5   6   .... n
+
+// 1   1
+
+// 2       1
+
+// 3           1
+
+// 4
+
+// 5
+
+// 6
+
+// .
+// .
+// .
+
+// n
