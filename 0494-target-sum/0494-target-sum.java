62370b9a1f7f027bb593dd2579ed67d7a81705a5
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2024-12-26 18:26:56 +0530

    Time: 33 ms (54.55%), Space: 54.9 MB (5.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,17 +1,21 @@
 class Solution {
-    private int addSubNoth(int id, int n, int[] nums, int target) {
+    private int addSubNoth(int[][] dp, int id, int n, int[] nums, int target) {
         if(n == id) {
             if(target == 0) return 1;
             return 0;
         }
 
-        int add = addSubNoth(id+1, n, nums, target - nums[id]);
-        int sub = addSubNoth(id+1, n, nums, target + nums[id]);
+        if(dp[id][target+1000] != -1) return dp[id][target+1000];
+
+        int add = addSubNoth(dp, id+1, n, nums, target - nums[id]);
+        int sub = addSubNoth(dp, id+1, n, nums, target + nums[id]);
         // int noth = addSubNoth(id+1, n, nums, target);
 
-        return add + sub;
+        return dp[id][target+1000] = add + sub;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return addSubNoth(0, nums.length, nums, target);
+        int[][] dp = new int[nums.length][21000+1];
+        for(int i = 0; i < nums.length; i++) Arrays.fill(dp[i], -1);
+        return addSubNoth(dp, 0, nums.length, nums, target);
     }
 }
