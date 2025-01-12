a2093c8ddb200eaac4b26c75b736a2086346c278
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-01-12 11:57:03 +0530

    Time: 148 ms (100%), Space: 75.3 MB (100%) - LeetHub

---
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    private int getMax(int[][][] dp, int[][] coins, int r, int c, int usedPowers) {
+        int m = coins.length;
+        int n = coins[0].length;
+
+        if(r == m || c == n) {
+            
+            return Integer.MIN_VALUE;
+        }
+
+        if(r == m-1 && c == n-1) {
+            if(coins[r][c] < 0 && usedPowers < 2) {
+                return 0;  // Can neutralize if powers left
+            }
+            return coins[r][c];
+        }
+
+        if(dp[r][c][usedPowers] != 1000000000) return dp[r][c][usedPowers];
+
+        int goRight = getMax(dp, coins, r, c+1, usedPowers);
+        int goDown = getMax(dp, coins, r+1, c, usedPowers);
+
+
+        int with = Math.max(goRight, goDown);
+
+        if(with != Integer.MIN_VALUE) with += coins[r][c];
+        int withOut = Integer.MIN_VALUE;
+
+        if(usedPowers < 2 && coins[r][c] < 0) {
+            goRight = getMax(dp, coins, r, c+1, usedPowers+1);
+            goDown = getMax(dp, coins, r+1, c, usedPowers+1);
+            withOut = Math.max(withOut, goDown);
+            withOut = Math.max(withOut, goRight);
+        }
+
+        // int withOut = Math.max(goRight, goDown);
+
+        return dp[r][c][usedPowers] = Math.max(with, withOut);
+    }
+
+    public int maximumAmount(int[][] coins) {
+        int m = coins.length;
+        int n = coins[0].length;
+        int[][][] dp = new int[m][n][3];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                Arrays.fill(dp[i][j], 1000000000);
+            }
+        }
+        return getMax(dp, coins, 0, 0, 0);
+    }
+}
+
