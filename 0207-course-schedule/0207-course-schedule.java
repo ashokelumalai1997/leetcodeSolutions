7a2a72f4aaf9851bcdc6ffda5cb8418e56e93f73
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-02-04 11:47:25 +0530

    Time: 7 ms (50.49%), Space: 45.5 MB (56.3%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,42 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
 
         for(int i = 0; i < numCourses; i++) {
-            adjList.put(i, new ArrayList<>());
+            adjList.add(new ArrayList<>());
         }
 
         int[] inDegree = new int[numCourses];
 
-        for(int[] course : prerequisites) {
-            adjList.get(course[1]).add(course[0]);
-            inDegree[course[0]]++;
+        for(int[] preReq : prerequisites) {
+            adjList.get(preReq[1]).add(preReq[0]);
+            inDegree[preReq[0]]++;
         }
 
         Queue<Integer> q = new LinkedList<>();
 
-        for(int i = 0; i < numCourses; i++) {
-            if(inDegree[i] == 0) q.offer(i);
-        }
-
-        int numComplete = 0;
-
-        while(!q.isEmpty()) {
-            int current = q.poll();
-
-            numComplete++;
-
-            for(int ne : adjList.get(current)) {
-                inDegree[ne]--;
-                if(inDegree[ne] == 0) q.offer(ne);
+        for(int i = 0; i < numCourses; i++){
+            if(inDegree[i] == 0) {
+                q.offer(i);
             }
         }
 
-        return numCourses == numComplete;
+        int processedCourses = 0;
 
+        while(!q.isEmpty()) {
+            int currentCourse = q.poll();
+
+
+            for(int nextCourse : adjList.get(currentCourse)) {
+                inDegree[nextCourse]--;
+                if(inDegree[nextCourse] == 0) {
+                    q.offer(nextCourse);
+                }
+            }
+            processedCourses++;
+        }
+
+        return processedCourses == numCourses;
 
 
     }
