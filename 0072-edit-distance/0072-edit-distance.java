680fe215cd6178fe18d5be54fcebc67f41543355
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-02-04 15:07:30 +0530

    Time: 6 ms (13.74%), Space: 45.3 MB (31.2%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,27 +1,32 @@
 class Solution {
-    private int editDistance(int[][] dp, int ind1, int ind2, String word1, String word2) {
-        if(ind1 == word1.length() && ind2 == word2.length()) return 0;
+    private int getMinOp(int[][] dp, int i1, int i2, String word1, String word2) {
+        if(i1 == word1.length()) {
+            return word2.length() - i2;
+        }
 
-        if(ind1 == word1.length()) return word2.length() - ind2;
-        if(ind2 == word2.length()) return word1.length() - ind1;
+        if(i2 == word2.length()) {
+            return word1.length() - i1;
+        }
 
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        if(dp[i1][i2] != -1) return dp[i1][i2];
 
-        if(word1.charAt(ind1) == word2.charAt(ind2)) 
-            return editDistance(dp, ind1+1, ind2+1, word1, word2);
-        
-        int replace = 1 + editDistance(dp, ind1+1, ind2+1, word1, word2);
-        int delete = 1 + editDistance(dp, ind1+1, ind2, word1, word2);
-        int insert = 1 + editDistance(dp, ind1, ind2+1, word1, word2);
+        if(word1.charAt(i1) == word2.charAt(i2)) {
+            return getMinOp(dp, i1+1, i2+1, word1, word2);
+        }
 
-        return dp[ind1][ind2] = Math.min(replace, Math.min(delete, insert));
+        int op1 = 1 + getMinOp(dp, i1, i2+1, word1, word2);
+        int op2 = 1 + getMinOp(dp, i1+1, i2, word1, word2);
+        int op3 = 1 + getMinOp(dp, i1 + 1, i2+1, word1, word2);
+
+        return dp[i1][i2] = Math.min(op1, Math.min(op2, op3));
     }
     public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-        int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++) Arrays.fill(dp[i], -1);
-        // if(word1.length() < word2.length()) return editDistance(0, 0, word2, word1);
-        return editDistance(dp, 0, 0, word1, word2);
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int[][] dp = new int[n1][n2];
+        for(int i = 0 ; i < n1; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return getMinOp(dp, 0, 0, word1, word2);
     }
 }
