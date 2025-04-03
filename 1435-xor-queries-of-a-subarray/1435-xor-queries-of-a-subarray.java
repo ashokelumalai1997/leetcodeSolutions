5c07b884eb92eb8e9a89daf6d521ef6b600e8fe3
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-04 00:27:58 +0530

    Time: 66 ms (14.25%), Space: 60.1 MB (94.22%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -1,23 +1,46 @@
 class Solution {
-    public int[] xorQueries(int[] arr, int[][] queries) {
-        int n = arr.length;
-        int totalXOR = 0;
-        int[] prefixXOR = new int[n];
-        int XORTracker = 0;
-        for(int i = 0; i < n; i++) {
-            totalXOR ^= arr[i];
-            XORTracker ^= arr[i];
-            prefixXOR[i] = XORTracker;
-        }
-        int queriesLength = queries.length;
-        int[] result = new int[queriesLength];
-        for(int i = 0; i < queriesLength; i++) {
-            int[] query = queries[i];
-            result[i] = totalXOR;
-            if(query[0] != 0) result[i] ^= prefixXOR[query[0]-1];
-            result[i] ^= (totalXOR ^ prefixXOR[query[1]]);
-        }
-        return result;
+	public int[] xorQueries(int[] arr, int[][] queries) {
+		int n = arr.length;
+		int[][] prefix = new int[n+1][32];
+		
+		for(int i = 1; i < n+1; i++) {
+			int index = 0;
+			int num = arr[i-1];
 
-    }
+            for (int b = 0; b < 32; b++) {
+                prefix[i][b] = prefix[i - 1][b];
+            }
+			
+			while(index < 32) {
+				if( ((1 << index) & num) > 0)  {
+					prefix[i][index]++;
+}
+if(i+1 < n+1)
+prefix[i+1][index] = prefix[i][index];
+index++;
+}
+}
+
+int q = queries.length;
+
+int[] res = new int[q];
+
+for(int i = 0; i < q; i++) {
+	int l = queries[i][0];
+	int r = queries[i][1];
+	int index = 0;
+	
+	int curr = 0;
+
+	int count = (r - l + 1);
+
+	while(index < 32) {
+		if( (prefix[r+1][index] - prefix[l][index])%2 == 1 ) curr |= (1 << index);
+		index++;
+}
+res[i] = curr;
+}
+
+return res;
+}
 }
