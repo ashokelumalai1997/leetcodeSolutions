33a8e802774f1fd714574f437242521b702f904f
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-05 21:15:43 +0530

    Time: 0 ms (100%), Space: 43.1 MB (10.64%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,24 +1,22 @@
 class Solution {
-    private void findAllSubsets(List<Integer> numsList, int index, List<Integer> tempList, List<List<Integer>> result){
-        //if(index==numsList.size()){
-            result.add(new ArrayList<>(tempList));
-          //  return;
-        //}
-        for(int i=index; i<numsList.size();i++){
-            tempList.add(numsList.get(i));
-            findAllSubsets(numsList, i+1, tempList, result);
-            tempList.remove(tempList.size()-1);
-            //findAllSubsets(numsList, i+1, tempList, result);
-        }
-    }
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> numsList = new ArrayList<>();
-        for(int num:nums){
-            numsList.add(num);
-        }
-        List<Integer> tempList = new ArrayList<>();
-        findAllSubsets(numsList, 0, tempList, result);
+	public void generateSubsets(int ind, int[] nums, List<Integer> current, List<List<Integer>> global) {
+		if(ind == nums.length) {
+			List<Integer> temp = new ArrayList<>(current);
+			global.add(temp);
+			return;
+}
+
+current.add(nums[ind]);
+generateSubsets(ind + 1, nums, current, global);
+current.remove(current.size()-1);
+generateSubsets(ind + 1, nums, current, global);
+}
+
+
+	public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> current  = new ArrayList<>();
+		generateSubsets(0, nums, current, result);
         return result;
-    }
+}
 }
