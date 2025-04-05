0694c882612b538d4f9429a055fe2624266d8c63
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-05 22:24:53 +0530

    Time: 2 ms (83.26%), Space: 44.7 MB (64.27%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,49 +1,25 @@
 class Solution {
-    private List<List<Integer>> comb(int ind, int[] candidates, int target) {
-        
-        if(target == 0) {
-            List<List<Integer>> res = new ArrayList<>();
-            res.add(new ArrayList<>());
-            return res;
-        }
-        if( ind == candidates.length) {
-            return new ArrayList<>();
-        }
+	public void generateCombinations(int ind, int[] candidates, int target, 
+List<Integer> combination, List<List<Integer>> result) {
+		if(target < 0 || ind == candidates.length) return;
+		if(target == 0)  {
+			List<Integer> temp = new ArrayList<>(combination);
+			result.add(temp);
+			return;
+}
 
-        // if(candidates[ind] >)
-        List<List<Integer>> skipAndMove = comb(ind+1, candidates, target);
+combination.add(candidates[ind]);
+// generateCombinations(ind, candidates, target - candidates[ind], combination, result);
+generateCombinations(ind , candidates, target - candidates[ind], combination, result);
+combination.remove(combination.size() - 1);
+generateCombinations(ind + 1, candidates, target, combination, result);
 
-        if(candidates[ind] == target) {
-            List<List<Integer>> n = new ArrayList<>();
-            List<Integer> l = new ArrayList<>();
-            l.add(candidates[ind]);
-            n.add(l);
-            n.addAll(skipAndMove);
-            return n;
-        }
 
-        List<List<Integer>> takeAndMove = new ArrayList<>();
-
-        if(candidates[ind] <= target) {
-            takeAndMove = comb(ind, candidates, target - candidates[ind]);
-            List<List<Integer>> temp = new ArrayList<>();
-            for(List<Integer> l : takeAndMove) {
-                List<Integer> n = new ArrayList<>();
-                n.add(candidates[ind]);
-                n.addAll(l);
-                temp.add(n);
-            }
-            takeAndMove = temp;
-        }
-
-        
-
-        takeAndMove.addAll(skipAndMove);
-        return takeAndMove;
-
-    }
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {   
-        return comb(0, candidates, target);
-        
-    }
+}
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> combination = new ArrayList<>();
+		generateCombinations(0, candidates, target, combination, result);
+		return result;
+}
 }
