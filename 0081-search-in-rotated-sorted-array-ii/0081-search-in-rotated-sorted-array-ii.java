5e11c8415eb7fea25cfcfa3cd3b2f6dd7c7d39fb
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-05 10:34:35 +0530

    Time: 0 ms (100%), Space: 43.7 MB (17.7%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,47 +1,26 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
-        int low = 0, high = nums.length-1;
-        int res = -1;
-        int mid;
-        while(low<=high){
-            mid=(low+high)/2;
-            //System.out.println("Low "+low+" high "+high + " mid "+mid);
-            
-            if(nums[low]==nums[high])
-            {
-                if(nums[low]==target)
-                {
-                    res=mid;
-                    break;
-                }
-                low+=1;
-                high-=1;
-                continue;
-            }
-            if(nums[mid]==target)
-            {
-                res=mid;
-                break;
-            }
-            if(nums[low]<=nums[mid]){
-                if(target<nums[mid]&&target>=nums[low]){
-                    high=mid-1;
-                    //continue;
-                } else {
-                    low=mid+1;
-                    //continue;
-                }
-                
-            } else {
-                if(target>nums[mid]&&target<=nums[high]){
-                    low=mid+1;
-                    //continue;
-                } else {
-                    high=mid-1;
-                    //continue;
-                }
-            }
+	boolean search(int[] nums, int target) {
+int n = nums.length;
+int left = 0;
+int right = n-1;
+
+while(left <= right) {
+	int mid = left + (right - left)/2;
+	if(target == nums[mid]) return true;
+        if(nums[left] == nums[right] && nums[mid] == nums[right]) {
+            left++;
+            right--;
+            continue;
         }
-        return res!=-1;
-    }
+	if(nums[mid] >= nums[left])  {
+		if(target >= nums[left] && target < nums[mid]) right = mid-1;
+		else left = mid+1;
+} else {
+	if(target > nums[mid] && target <= nums[right]) left = mid+1;
+	else right = mid-1;
+}
+}
+
+return false;
+}
 }
