ee365dc9357b751fe932bf1cf3f3dfa1b7309d27
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-08 08:35:45 +0530

    Time: 2 ms (72.1%), Space: 44.4 MB (50.97%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,31 +1,13 @@
 class Solution {
-    public int minimumOperations(int[] nums) {
-        Map<Integer, Integer> mp = new HashMap<>();
-
-        for(int num : nums) {
-            mp.putIfAbsent(num, 0);
-            mp.put(num, mp.get(num)+1);
-        }
-
-        int tracker = 0;
-        int ops = 0;
-
-        int n = nums.length;
-
-        while(tracker < n && mp.size() != (n)-tracker) {
-            ops++;
-            for(int i = 0; tracker < n && i < 3; i++) {
-                mp.put(nums[tracker], mp.get(nums[tracker])-1);
-                if(mp.get(nums[tracker]) == 0) {
-                    mp.remove(nums[tracker]);
-                }
-                tracker++;
-            }
-        }
-
-
-        return ops;
-
-        
-    }
+	public int minimumOperations(int[] nums) {
+		int n = nums.length;
+		int right = n-1;
+        Set<Integer> st = new HashSet<>();
+		while(right >= 0 && !st.contains(nums[right])) {
+            st.add(nums[right]);
+			right--;
+}
+if(right == -1) return 0;
+return ((right+1)/3) + (((right + 1)%3 == 0) ? 0 : 1);
+}
 }
