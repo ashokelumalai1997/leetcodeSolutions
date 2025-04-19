e7f30c25a6ba000e5cb5629ac7a2904e9eb22617
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-20 00:15:13 +0530

    Time: 3 ms (3.06%), Space: 44.9 MB (26.34%) - LeetHub

--- 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.java
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.java
@@ -13,77 +13,115 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        
+//     }
+// }
+
+
 class Solution {
-    TreeNode roota = null;
-    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
-        roota = root;
-        List<Integer> result = new ArrayList<>();
-        if (root == null) return result;
+
+// 	static class TreeNode {
+// 		int val;
+// 		TreeNode left;
+// 		TreeNode right;
+
+// 		public TreeNode() {
+// 			//TODO :
+// }
+// }
+
+	public List<Integer> boundaryOfBinaryTree(TreeNode node) {
+
         
-        // Add root
-        result.add(root.val);
-        
-        // If root is a leaf node, return early
-        if (root.left == null && root.right == null) return result;
-        
-        // Get left boundary (excluding leaf nodes)
-        getLeftBoundary(root.left, result);
-        
-        // Get leaf nodes
-        getLeaves(root, result);
-        
-        // Get right boundary in reverse order (excluding leaf nodes)
-        List<Integer> rightBoundary = new ArrayList<>();
-        getRightBoundary(root.right, rightBoundary);
-        Collections.reverse(rightBoundary);
-        result.addAll(rightBoundary);
-        
-        return result;
-    }
-    
-    private void getLeftBoundary(TreeNode node, List<Integer> result) {
-        if (node == null) return;
-        
-        // If node is not a leaf, add to result
-        if (!(node.left == null && node.right == null)) {
-            result.add(node.val);
-        }
-        
-        // Traverse left if possible, otherwise right
-        if (node.left != null) {
-            getLeftBoundary(node.left, result);
-        } else if (node.right != null) {
-            getLeftBoundary(node.right, result);
-        }
-    }
-    
-    private void getRightBoundary(TreeNode node, List<Integer> result) {
-        if (node == null) return;
-        
-        // If node is not a leaf, add to result
-        if (!(node.left == null && node.right == null)) {
-            result.add(node.val);
-        }
-        
-        // Traverse right if possible, otherwise left
-        if (node.right != null) {
-            getRightBoundary(node.right, result);
-        } else if (node.left != null) {
-            getRightBoundary(node.left, result);
-        }
-    }
-    
-    private void getLeaves(TreeNode node, List<Integer> result) {
-        if (node == null) return;
-        
-        // If node is a leaf, add to result
-        if (node.left == null && node.right == null && node != roota) {
-            result.add(node.val);
-            return;
-        }
-        
-        // Traverse left and right subtrees
-        getLeaves(node.left, result);
-        getLeaves(node.right, result);
-    }
+		ArrayList<Integer> leftBoundary = new ArrayList<>();
+		if(node.left != null) leftBoundary = getLeftBoundary(node.left);
+		ArrayList<Integer> rightBoundary = new ArrayList<>();
+if(node.right != null) rightBoundary = getRightBoundary(node.right);
+		ArrayList<Integer> bottomBoundary = getBottomBoundary(node);
+
+		// if(leftBoundary.size() != 0)
+        // leftBoundary.remove(leftBoundary.size() - 1);
+
+		 
+		ArrayList<Integer> result = new ArrayList<>();
+
+		result.add(node.val);
+
+        if(node.left == null && node.right == null) return result;
+
+		Collections.reverse(rightBoundary);
+		// rightBoundary.remove(0);
+		
+		result.addAll(leftBoundary);
+		result.addAll(bottomBoundary);
+		result.addAll(rightBoundary);
+
+
+		return result;
+
+}
+
+private ArrayList<Integer> getLeftBoundary(TreeNode node) {
+	Deque<TreeNode> q = new LinkedList<>();
+	q.offer(node);
+	ArrayList<Integer> result = new ArrayList<>();
+	while(!q.isEmpty()) {
+		TreeNode current  = q.peek();
+        if(!(current.left == null && current.right == null))
+		result.add(current.val);
+
+		int size = q.size();
+		while(size != 0) {
+			TreeNode qcurrent = q.poll();
+			if(qcurrent.left != null)
+			q.offer(qcurrent.left);
+			else if(qcurrent.right != null)
+			q.offer(qcurrent.right);
+			size--;
+}
+}
+
+return result;
+}
+
+private ArrayList<Integer> getRightBoundary(TreeNode node) {
+	Deque<TreeNode> q = new LinkedList<>();
+	q.offer(node);
+	ArrayList<Integer> result = new ArrayList<>();
+	while(!q.isEmpty()) {
+		TreeNode current  = q.peekLast();
+        if(!(current.left == null && current.right == null))
+		result.add(current.val);
+
+		int size = q.size();
+		while(size != 0) {
+			TreeNode qcurrent = q.poll();
+			if(qcurrent.right != null)
+			q.offer(qcurrent.right);
+				else if(qcurrent.left != null)
+			q.offer(qcurrent.left);
+			size--;
+}
+}
+
+return result;
+}
+
+private ArrayList<Integer> getBottomBoundary(TreeNode node) {
+	ArrayList<Integer> result = new ArrayList<>();
+if(node == null) return result;
+
+	if(node.left == null && node.right == null) {
+		result.add(node.val);
+		return result;
+}
+
+
+result.addAll(getBottomBoundary(node.left));
+result.addAll(getBottomBoundary(node.right));
+
+return result;
+}
 }
