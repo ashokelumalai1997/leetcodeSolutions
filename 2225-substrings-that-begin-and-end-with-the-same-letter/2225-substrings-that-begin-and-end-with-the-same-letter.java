6aaf336ceb6cfbc50060faf1ba4c174118cea983
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-20 12:08:35 +0530

    Time: 3 ms (96.88%), Space: 45.1 MB (56.25%) - LeetHub

--- 2225-substrings-that-begin-and-end-with-the-same-letter/2225-substrings-that-begin-and-end-with-the-same-letter.java
+++ 2225-substrings-that-begin-and-end-with-the-same-letter/2225-substrings-that-begin-and-end-with-the-same-letter.java
@@ -1,11 +1,12 @@
 class Solution {
-    public long numberOfSubstrings(String s) {
-        long result = 0;
-        long[] freq = new long[26];
-        for(char ch : s.toCharArray()) {
-            freq[ch-'a']++;
-            result+=freq[ch-'a'];
-        }
-        return result;
-    }
+	public long numberOfSubstrings(String s) {
+		int[] freq = new int[26];
+		long count = 0;
+		for(char ch : s.toCharArray()) {
+			freq[ch - 'a']++;
+			count += freq[ch - 'a'];
+}
+
+return count;
+} 
 }
