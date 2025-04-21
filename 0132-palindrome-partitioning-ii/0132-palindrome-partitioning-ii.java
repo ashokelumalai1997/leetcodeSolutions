19a1f7be5b45694ef8a20078d43d3c6656a73eb3
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-21 23:14:23 +0530

    Time: 49 ms (62.73%), Space: 77.2 MB (5.16%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,37 +1,50 @@
 class Solution {
-    public int minCut(String s) {
-        int n = s.length();
-        boolean[][] isPal = new boolean[n][n];
+	public int minCut(String s) {
+		int n = s.length();
 
-        // Fill isPal[i][j]
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    if (j - i <= 1) {
-                        isPal[i][j] = true;
-                    } else {
-                        isPal[i][j] = isPal[i + 1][j - 1];
-                    }
-                }
-            }
-        }
+		boolean[][] isPal = new boolean[n][n];
 
-        // dp[i] = min cuts needed for s[0..i]
-        int[] dp = new int[n];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+		for(int i = 0; i < n; i++) {
+			isPal[i][i] = true;
+			if(i+1 < n && s.charAt(i) == s.charAt(i+1)) isPal[i][i+1] = true;
+}
+		
+		for(int i = 2; i < n; i++) {
+			for(int j = 0; j + i < n; j++) {
+				if(s.charAt(j) == s.charAt(j + i)) {
+					isPal[j][j + i] = isPal[j+1][j+i-1];
+}
+}
+}
 
-        for (int i = 0; i < n; i++) {
-            if (isPal[0][i]) {
-                dp[i] = 0; // no cut needed
-            } else {
-                for (int j = 1; j <= i; j++) {
-                    if (isPal[j][i]) {
-                        dp[i] = Math.min(dp[i], dp[j - 1] + 1);
-                    }
-                }
-            }
-        }
+int[][] dp = new int[n][n];
 
-        return dp[n - 1];
-    }
+// for(int[] d : dp) {
+// 	Arrays.fill(d, Integer.MAX_VALUE);
+// }
+
+// dp[i][j] = if(j to i is pal) min(1 + dp[i+1][i+1], dp[i+1][j])
+			// else dp[i+1][j]
+
+for(int i = 0; i < n; i++) {
+	if(isPal[i][n-1])
+	dp[n-1][i] = 0;
+else 
+	dp[n-1][i] = Integer.MAX_VALUE;
+}
+
+for(int i = n-2; i >= 0; i--) {
+	for(int j = 0; j < n; j++) {
+		if(isPal[j][i] && dp[i+1][i+1] != Integer.MAX_VALUE) {
+			dp[i][j] = Math.min(1 + dp[i+1][i+1], dp[i+1][j]);
+			
+} else {
+	dp[i][j] = dp[i+1][j];
+}
+		
+}
+}
+
+return dp[0][0];
+}
 }
