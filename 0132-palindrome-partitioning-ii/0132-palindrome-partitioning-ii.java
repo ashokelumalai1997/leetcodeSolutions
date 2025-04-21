e997da8bc587bef4beba665cb30359ee3a16ef34
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-21 22:43:21 +0530

    Time: 88 ms (57.06%), Space: 77.6 MB (5.16%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,45 +1,56 @@
 class Solution {
+	public int minCut(String s) {
+		int n = s.length();
+		boolean[][] isPal = new boolean[n][n];
+		
+		for(int i = 0; i < n; i++) {
+			isPal[i][i] = true;
+}
 
-    private boolean isPal(int start, int end, String s, int[][] palMemo) {
-        if(palMemo[start][end] != -1) return palMemo[start][end] == 1;
-        if(start >= end) return true;
-
-        boolean res = s.charAt(start) == s.charAt(end) && 
-                                isPal(start+1, end-1, s, palMemo);
-
-        palMemo[start][end] = res ? 1 : 0;
-
-        return palMemo[start][end] == 1;
-    }
-    private int cutOrContinue(int ind, int palStart, String s, int[][] memo, int[][] palMemo) {
-        if(ind == s.length()) {
-            if(isPal(palStart, ind-1, s, palMemo)) return 0;
-            return Integer.MAX_VALUE;
-        }
-
-        if(memo[ind][palStart] != -1) return memo[ind][palStart];
-
-        int cut = Integer.MAX_VALUE;
-
-        if(isPal(palStart, ind, s, palMemo)) {
-            cut = cutOrContinue(ind+1, ind+1, s, memo, palMemo);
-            if(cut != Integer.MAX_VALUE) cut++;
-        }
-
-        int cont = cutOrContinue(ind+1, palStart, s, memo, palMemo);
-        return memo[ind][palStart] = Math.min(cut, cont);
-    }
-    public int minCut(String s) {
-        int n = s.length();
-        int[][] memo = new int[n][n];
-        int[][] palMemo = new int[n+1][n];
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(memo[i], -1);
-            Arrays.fill(palMemo[i], -1);
-        }
-        return cutOrContinue(0, 0, s, memo, palMemo);
-    }
-        
+for(int i = 0; i < n - 1; i++) {
+	if(s.charAt(i) == s.charAt(i+1)) {
+		isPal[i][i+1]  = true;
+}
 }
 
 
+
+for(int i = 2; i < n; i++) {
+	for(int j = 0; j + i < n; j++) {
+		if(s.charAt(j) == s.charAt(i+j)) {
+			isPal[j][i+j] = isPal[j+1][i+j-1];
+}
+}
+}
+
+int[][] dp = new int[n][n];
+
+for(int[] d : dp) {
+	Arrays.fill(d, -1);
+}
+
+return getPart(dp, 0, 0, s, isPal) - 1;
+}
+
+
+
+private int getPart(int[][] dp, int start, int ind, String s, boolean[][] isPal) {
+	if(start == s.length()) return 0;
+
+	if(ind == s.length()) {
+		return Integer.MAX_VALUE;
+    }
+
+    if(dp[start][ind] != -1) return dp[start][ind];
+
+    int cont = getPart(dp, start, ind+1, s, isPal);
+    int brea = Integer.MAX_VALUE;
+    if(isPal[start][ind]) {
+	    int temp = getPart(dp, ind+1, ind+1, s, isPal);
+	    if(temp != Integer.MAX_VALUE)
+	    brea = 1 + temp;
+    }
+
+    return dp[start][ind] = Math.min(cont, brea);
+}
+}
