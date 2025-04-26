d0831e1d2a07dfcff7b02c2a9065ee6d508d3db9
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-26 22:54:56 +0530

    Time: 51 ms (17.28%), Space: 56.8 MB (99.48%) - LeetHub

--- 3821-count-cells-in-overlapping-horizontal-and-vertical-substrings/3821-count-cells-in-overlapping-horizontal-and-vertical-substrings.java
+++ 3821-count-cells-in-overlapping-horizontal-and-vertical-substrings/3821-count-cells-in-overlapping-horizontal-and-vertical-substrings.java
@@ -1,94 +1,116 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
 
-    private String getHString(char[][] grid) {
-        StringBuilder sb = new StringBuilder();
+	private String getHString(char[][] grid) {
+		StringBuilder sb = new StringBuilder();
         int m = grid.length;
         int n = grid[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                sb.append(grid[i][j]);
-            }
-        }
-        return sb.toString();
-    }
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				sb.append(grid[i][j]);
+}
+}
 
-    private String getVString(char[][] grid) {
-        StringBuilder sb = new StringBuilder();
-        int m = grid.length;
+        // System.out.println(sb);
+
+return sb.toString();
+}
+
+private String getVString(char[][] grid) {
+		StringBuilder sb = new StringBuilder();
+    int m = grid.length;
         int n = grid[0].length;
-        for (int j = 0; j < n; j++) { // fixed column-major order
-            for (int i = 0; i < m; i++) {
-                sb.append(grid[i][j]);
-            }
-        }
-        return sb.toString();
-    }
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				sb.append(grid[j][i]);
+}
+}
+    // System.out.println(sb);
 
-    private int[] getKMP(String text, String pat) {
-        int t = text.length();
-        int p = pat.length();
+return sb.toString();
+}
 
-        int[] KMPArr = new int[t + p + 1];
-        String newPat = pat + '$' + text;
-        int n = newPat.length();
+private int[] getKMP(String text, String pat) {
+	int t = text.length();
+	int p = pat.length();
+	
+	int[] KMPArr = new int[t + p+1];
 
-        int i = 0, j = 1;
-        while (j < n) {
-            if (newPat.charAt(i) == newPat.charAt(j)) {
-                KMPArr[j] = i + 1;
-                i++;
-                j++;
-            } else if (i != 0) {
-                i = KMPArr[i - 1];
-            } else {
-                j++;
-            }
-        }
+	int n = t+p+1;
+	
+	int i = 0;
+	int j = 1;
 
-        int[] res = new int[t];
-        for (int o = p + 1; o < t + p + 1; o++) {
-            res[o - p - 1] = KMPArr[o];
-        }
-        return res;
-    }
+	String newPat = pat + '$' +  text;
 
-    public int countCells(char[][] grid, String pattern) {
-        String hString = getHString(grid);
-        String vString = getVString(grid);
-        int[] hKMP = getKMP(hString, pattern);
-        int[] vKMP = getKMP(vString, pattern);
+	while(j < n) {
+		if(newPat.charAt(i) == newPat.charAt(j)) {
+			KMPArr[j] = i+1;
+			i++;
+			j++;
+} else if(i != 0) {
+	i = KMPArr[i-1];
+} else {
+j++;
+}
 
-        int m = grid.length;
-        int n = grid[0].length;
-        int p = pattern.length();
+        
 
-        Set<Integer> positions = new HashSet<>();
-        int lastValidPosition = Integer.MAX_VALUE;
+        
+}
 
-        for (int i = m*n-1; i >= 0; i--) {
-            if (hKMP[i] == p) lastValidPosition = i;
-            if (lastValidPosition <= i + p - 1) {
-                int rId = i / n;
-                int cId = i % n;
-                positions.add(rId * n + cId); // FIX: always row * n + col
-            }
-        }
+int[] res = new int[t];
+for(int o = p+1; o < t+p+1; o++) {
+	res[o - p-1] = KMPArr[o];
+    // System.out.print(res[o-p-1]);
+}
+    // System.out.println();
+    // System.out.println(newPat);
 
-        lastValidPosition = Integer.MAX_VALUE;
-        int count = 0;
+return res;
+}
 
-        for (int i = m*n-1; i >= 0; i--) {
-            if (vKMP[i] == p) lastValidPosition = i;
-            if (lastValidPosition <= i + p - 1) {
-                int rId = i % m;
-                int cId = i / m;
-                if (!positions.add(rId * n + cId)) count++; // FIX: again row * n + col
-            }
-        }
 
-        return count;
-    }
+	public int countCells(char[][] grid, String pattern) {
+		String hString = getHString(grid);
+		String vString = getVString(grid);
+		int[] hKMP = getKMP(hString, pattern);
+		int[] vKMP = getKMP(vString, pattern);
+		
+		int m = grid.length;
+		int n = grid[0].length;
+		
+		Set<Integer> positions = new HashSet<>();
+
+		int lastValidPosition = Integer.MAX_VALUE;
+
+		int p = pattern.length();
+		
+for(int i = m*n-1; i >= 0; i--) {
+			if(hKMP[i] == p) lastValidPosition = i;
+			if(lastValidPosition <= i + p - 1) {
+				int rId = ((i)/n);
+				int cId = ((i)%n);
+				positions.add(rId*n + cId);
+}
+}
+
+lastValidPosition = Integer.MAX_VALUE;
+
+int count = 0;
+
+        // for(int pa : positions) {
+        //     System.out.println(pa);
+        // }
+
+for(int i = m*n-1; i >= 0; i--) {
+			if(vKMP[i] == p) lastValidPosition = i;
+			if(lastValidPosition <= i + p - 1) {
+				int cId = ((i)/m);
+				int rId = ((i)%m);
+				if(!positions.add(rId*n + cId)) count++;
+}
+}
+
+return count;
+}
 }
