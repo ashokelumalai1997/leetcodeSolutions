2346ae26059cbd4dd163decea91efa9ef5c5d2f3
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-04-28 16:40:47 +0530

    Time: 23 ms (36.04%), Space: 45.4 MB (20.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,37 +1,31 @@
 class Solution {
 	public int coinChange(int[] coins, int amount) {
 		int n = coins.length;
-		int[][] dp = new int[n][amount+1];
-
-		for(int[] d : dp) Arrays.fill(d, -1);
-        
-
-        int res = f(0, coins, amount, dp);
-
-        if(res != Integer.MAX_VALUE) return res;
+		int[][] dp = new int[n][amount + 1];
 		
-		return -1;
+		for(int i = 1; i <= amount; i++) {
+			dp[n-1][i] = Integer.MAX_VALUE;
 }
 
-private int f(int ind, int[] nums, int amount, int[][] dp) {
-	if(amount == 0) return 0;
-	if(ind == nums.length)  return Integer.MAX_VALUE;
 
-    if(dp[ind][amount] != -1) return dp[ind][amount];
-	int take = Integer.MAX_VALUE;
-	int notTake = Integer.MAX_VALUE;
-	
-	int currentC = nums[ind];
-	int currentA = amount;
-	
-	for(int i = 1; currentA - currentC*i >= 0; i++ ) {
-		int next = f(ind + 1, nums, amount - currentC*i, dp);
-		if(next == Integer.MAX_VALUE) continue;
-take = Math.min(take, i + next);
+
+for(int i = n-1; i >= 0; i-- ) {
+	for(int j = 1; j <= amount; j++) {
+
+		int next = Integer.MAX_VALUE;
+
+		if(i != n-1) {
+			next = dp[i+1][j];
+}
+		if(coins[i] > j || dp[i][j-coins[i]] == Integer.MAX_VALUE) {
+			dp[i][j] = next;
+			continue;
+}
+		dp[i][j] = Math.min(next
+, 1 + dp[i][j-coins[i]]);
+}
 }
 
-notTake = f(ind+1, nums, amount, dp);
-
-return dp[ind][amount] = Math.min(take, notTake);
+return dp[0][amount] == Integer.MAX_VALUE ? -1 : dp[0][amount];
 }
 }
