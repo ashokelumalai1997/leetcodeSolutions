d4cfabddeb9479231633fb3572f394b8c94743ab
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-05-07 09:09:04 +0530

    Time: 6 ms (98.75%), Space: 44.7 MB (83.49%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.java
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.java
@@ -1,63 +1,55 @@
 class Solution {
-    public int minTimeToReach(int[][] moveTime) {
+	public int minTimeToReach(int[][] moveTime) {
+		int m = moveTime.length;
+		int n = moveTime[0].length;
+		
 
-        // int temp = moveTime[0][0];
-        int m = moveTime.length;
-        int n = moveTime[0].length;
-        int[][] res = new int[m][n];
+		// int[] - array of size 3 , 0 - row, 1 - col, 2 - distance
+		PriorityQueue<int[]> pq = new PriorityQueue<>(
+(a, b) -> a[2] - b[2]
+);
 
-        for(int i = 0; i < m; i++) Arrays.fill(res[i], Integer.MAX_VALUE);
+		pq.offer(new int[]{0, 0, 0});
 
-        PriorityQueue<int[]> pq 
-                = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-        
-        pq.offer(new int[]{0, 0, 0});
+		int minDist = 0;
 
-        while(!pq.isEmpty()) {
-            int[] currentNode = pq.poll();
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
 
-            int row = currentNode[0];
-            int col = currentNode[1];
-            int dist = currentNode[2];
+        int[][] moveDist = new int[m][n];
 
-            if(row+1 < m) {
-                if(1+dist < res[row+1][col]) {
-                res[row+1][col] = Math.max(1+moveTime[row+1][col], Math.min(res[row+1][col], 1+dist));
-                pq.offer(new int[]{row+1, col, res[row+1][col]});
-                }
-            }
-            
+        for(int[] a : moveDist) Arrays.fill(a, Integer.MAX_VALUE);
 
-            if(col+1 < n) {
-                if(1+dist < res[row][col+1]) {
-                res[row][col+1] = Math.max(1+moveTime[row][col+1], Math.min(res[row][col+1], 1+dist));
-                pq.offer(new int[]{row, col+1, res[row][col+1]});
-                }
-            }
-
-            if(row-1 >= 0) {
-                if(1+dist < res[row-1][col]) {
-                res[row-1][col] = Math.max(1+moveTime[row-1][col], Math.min(res[row-1][col], 1+dist));
-                pq.offer(new int[]{row-1, col, res[row-1][col]});
-                }
-            }
-            
-
-            if(col-1 >= 0) {
-                if(1+dist < res[row][col-1]) {
-                    res[row][col-1] = Math.max(1+moveTime[row][col-1], Math.min(res[row][col-1], 1+dist));
-                    pq.offer(new int[]{row, col-1, res[row][col-1]});
-                }
-            }
-        }
-
-        return res[m-1][n-1];
-
-        
-    }
+        moveDist[0][0] = 0;
+		
+		while(!pq.isEmpty()) {
+			int[] current  = pq.poll();
+			
+			int row = current[0];
+			int col = current[1];
+			int dist = current[2];
+	
+			if(row == m-1 && col == n-1) {
+				minDist = dist;
+				break;
 }
 
+for(int[] dir : directions) {
+	int nextRow = row + dir[0];
+	int nextCol = col + dir[1];
+	
+	if(nextRow < m && nextRow >= 0 
+&& nextCol < n && nextCol >= 0) {
 
-    // 1.   2.  3.  4
-    // 5.   6.  7.  8
-    // 9.  10. 11. 12
+int nextDist = Math.max(1 + moveTime[nextRow][nextCol], dist + 1);
+
+if(moveDist[nextRow][nextCol] <= nextDist) continue;
+pq.offer(new int[]{nextRow, nextCol, nextDist});
+moveDist[nextRow][nextCol] = nextDist;
+}
+}
+}
+
+return minDist;
+
+}
+}
