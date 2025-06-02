7bad66f13a202696fb15b69593491ef176921ebf
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-06-02 22:42:06 +0530

    Time: 3 ms (89.07%), Space: 61.5 MB (48.78%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,71 +1,35 @@
 class Solution {
-    private int profit(int[][][] dp, int ind, int sign, int needed, int[] prices) {
-        if(needed == 0) return 0;
-
-        if(ind == prices.length) return Integer.MIN_VALUE;
-
-        if(dp[ind][sign][needed] != -1) return dp[ind][sign][needed];
-
-        int multiplier = (sign == 0) ? -1 : 1;
-
-        int nextSign = (sign == 1) ? 0 : 1;
-
-        int take = 0;
-
-        int next = profit(dp, ind+1, nextSign, needed-1, prices);
-
-        if(next != Integer.MIN_VALUE) take = prices[ind]*multiplier + next;
-
-        int notTake = profit(dp, ind+1, sign, needed, prices);
-
-        return dp[ind][sign][needed] = Math.max(take, notTake);
-    }
     public int maxProfit(int[] prices) {
+        int length = prices.length;
+        if (length <= 1) return 0;
 
-        int n = prices.length;
+        int leftMin = prices[0];
+        int rightMax = prices[length - 1];
 
-        int[][][] dp = new int[n][2][5];
+        int[] leftProfits = new int[length];
+        // pad the right DP array with an additional zero for convenience.
+        int[] rightProfits = new int[length + 1];
 
-        for(int i = 0; i < n; i ++) {
-            for(int j = 0; j < 2; j++) {
-                Arrays.fill(dp[i][j], -1);
-            }
+        // construct the bidirectional DP array
+        for (int l = 1; l < length; ++l) {
+            leftProfits[l] = Math.max(leftProfits[l - 1], prices[l] - leftMin);
+            leftMin = Math.min(leftMin, prices[l]);
+
+            int r = length - 1 - l;
+            rightProfits[r] = Math.max(
+                rightProfits[r + 1],
+                rightMax - prices[r]
+            );
+            rightMax = Math.max(rightMax, prices[r]);
         }
 
-        int pro = profit(dp, 0, 0, 4, prices);
-        pro = (pro < 0) ? 0 : pro;
-
-        int[][][] dp1 = new int[n][2][3];
-
-        for(int i = 0; i < n; i ++) {
-            for(int j = 0; j < 2; j++) {
-                Arrays.fill(dp1[i][j], -1);
-            }
+        int maxProfit = 0;
+        for (int i = 0; i < length; ++i) {
+            maxProfit = Math.max(
+                maxProfit,
+                leftProfits[i] + rightProfits[i]
+            );
         }
-
-
-        int pro1 = profit(dp1, 0, 0, 2, prices);
-        pro1 = (pro1 <0) ? 0 : pro1;
-
-        
-
-        return Math.max(pro, pro1);
+        return maxProfit;
     }
 }
-
-
-// - + - +
-
-
-// if(needed = 0) return 0;
-// if(i == n) return MIN
-
-
-// f(i, 0, 4) -> Max (f(i+1,0,4) and f(i+1, 1, 3) )
-
-// f(i,0, 4) maximum sum starting at index i if num[i] is negatively added
-// f(i, 1,4) maximum sum starting at index i if num[i] is positively added
-
-
-// 3  5 1 2 3
-
