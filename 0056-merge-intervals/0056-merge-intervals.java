4a44ee0d5ebec08eec52bcba3695104505d4ab92
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-06-03 06:34:48 +0530

    Time: 7 ms (97.95%), Space: 46.6 MB (55.13%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,44 @@
+class Solution {
+
+	static class ArrayComparator implements Comparator<int[]> {
+		public int compare(int[] a, int[] b) {
+			if(a[0] == b[0]) return a[1] - b[1];
+			return a[0] - b[0];
+}
+}
+	public int[][] merge(int[][] intervals) {
+		int n = intervals.length;
+
+		if(n == 0) return new int[][]{};
+
+		Arrays.sort(intervals, new ArrayComparator());
+
+int[] prev = intervals[0];
+
+List<int[]> out = new ArrayList<>();
+
+for(int[] current : intervals) {
+	if(prev[1] >= current[0]) {
+		prev[1] = Math.max(current[1], prev[1]);
+} else {
+	out.add(prev);
+	prev = current;
+}
+}
+
+out.add(prev);
+
+int o = out.size();
+
+int[][] outArr = new int[o][2];
+
+for(int i = 0; i < o; i++) {
+	outArr[i][0] = out.get(i)[0];
+	outArr[i][1] = out.get(i)[1];
+}
+
+return outArr;
+
+}
+}
+
