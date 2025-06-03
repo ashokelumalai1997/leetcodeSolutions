ff123c989fdbf929578b1bd130e7235b0b43ff5a
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-06-03 09:29:25 +0530

    Time: 6 ms (74.06%), Space: 45.3 MB (69.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,43 +1,41 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		
+		Queue<Integer> courseQ = new LinkedList<>();
+		
+		int[] inDegree = new int[numCourses];
+		
+		List<List<Integer>> adjlist = new ArrayList<>();
 
-        for(int i = 0; i < numCourses; i++) {
-            adjList.add(new ArrayList<>());
-        }
+		for(int i = 0; i < numCourses; i++) {
+			adjlist.add(new ArrayList<>());
+}
+		
+		for(int[] dep : prerequisites) {
+			adjlist.get(dep[1]).add(dep[0]);
+			inDegree[dep[0]]++;
+}
 
-        int[] inDegree = new int[numCourses];
+for(int i = 0; i < numCourses; i++) {
+	if(inDegree[i] == 0) {
+		courseQ.add(i);
+}
+}
+int completed = 0;
 
-        for(int[] preReq : prerequisites) {
-            adjList.get(preReq[1]).add(preReq[0]);
-            inDegree[preReq[0]]++;
-        }
+while(!courseQ.isEmpty()) {
+	completed++;
+	int current = courseQ.poll();
+	
+	for(int next : adjlist.get(current)) {
+		inDegree[next]--;
+		if(inDegree[next] == 0) {
+			courseQ.add(next);
+}
+}
+}
 
-        Queue<Integer> q = new LinkedList<>();
+return (completed == numCourses);
+}
 
-        for(int i = 0; i < numCourses; i++){
-            if(inDegree[i] == 0) {
-                q.offer(i);
-            }
-        }
-
-        int processedCourses = 0;
-
-        while(!q.isEmpty()) {
-            int currentCourse = q.poll();
-
-
-            for(int nextCourse : adjList.get(currentCourse)) {
-                inDegree[nextCourse]--;
-                if(inDegree[nextCourse] == 0) {
-                    q.offer(nextCourse);
-                }
-            }
-            processedCourses++;
-        }
-
-        return processedCourses == numCourses;
-
-
-    }
 }
