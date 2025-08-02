46f05b7e1ac9fec5e5cac5c4ef747c90e8ff128a
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-08-02 10:46:16 +0530

    Time: 0 ms (100%), Space: 41.8 MB (88.71%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        
+        return pow(x, n);
+    }
+
+    public double pow(double x, long n) {
+        
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        boolean neg = false;
+        if(n < 0) {
+            return 1/pow(x, -1*n);
+        }
+        double res = pow(x, n/2);
+        double result = 0;
+        if(n%2 == 0) return res*res;
+        result = res*res*x;
+
+        return result;
     }
 }
