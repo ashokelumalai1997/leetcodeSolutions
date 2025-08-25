7032bd1a2b6997ded93ea1cb13ce77ddfeeda932
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-08-25 15:45:57 +0530

    Time: 49 ms (38.06%), Space: 114.4 MB (55.09%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -2,52 +2,27 @@ class LRUCache {
 
     static class ListNode {
         int key;
-        int val;
-
+        int value;
         ListNode prev;
         ListNode next;
 
         public ListNode(int key, int val) {
             this.key = key;
-            this.val = val;
-            this.prev = null;
-            this.next = null;
+            this.value = val;
+            prev = null;
+            next = null;
         }
     }
 
     static class DoublyLinkedList {
         ListNode head;
         ListNode tail;
-
         public DoublyLinkedList() {
             this.head = null;
             this.tail = null;
         }
 
-        public void remove(ListNode node) {
-            if(this.head == node) {
-                this.head = this.head.next;
-                if(this.head != null) this.head.prev = null;
-                node.prev = null;
-                node.next = null;
-                return;
-            }
-
-            if(this.tail == node) {
-                this.tail = this.tail.prev;
-                this.tail.next = null;
-                node.prev = null;
-                node.next = null;
-                return;
-            }
-
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-            node.next = null;
-            node.prev = null;
-        }
-
-        public void addLast(ListNode node) {
+        public void addNode(ListNode node) {
             if(this.head == null) {
                 this.head = node;
                 this.tail = node;
@@ -58,48 +33,87 @@ class LRUCache {
             node.prev = this.tail;
             this.tail = this.tail.next;
         }
+
+        public void removeNode(ListNode node) {
+            if(this.head == this.tail) {
+                this.head = null;
+                this.tail = null;
+                return;
+            }
+            if(this.tail == node) {
+                this.tail = this.tail.prev;
+                this.tail.next = null;
+                node.prev = null;
+                node.next = null;
+                return;
+            }
+
+            if(this.head == node) {
+                this.head = this.head.next;
+                this.head.prev = null;
+                node.prev = null;
+                node.next = null;
+                return;
+            }
+
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            node.prev = null;
+            node.next = null;
+            return;
+        }
     }
+
 
     int cap;
-    Map<Integer, ListNode> hm;
     DoublyLinkedList dll;
-
+    Map<Integer, ListNode> mp;
     public LRUCache(int capacity) {
         this.cap = capacity;
-        this.hm = new HashMap<>();
         this.dll = new DoublyLinkedList();
+        this.mp = new HashMap<>();
     }
     
+    /*
+    return -1 if key is not there in hm
+    get node from hm
+    remove it and add it to end
+    return the value
+    */
     public int get(int key) {
-        if(!this.hm.containsKey(key)) {
+        if(!this.mp.containsKey(key)) {
             return -1;
         }
 
-        ListNode node = this.hm.get(key);
-
-        this.dll.remove(node);
-        this.dll.addLast(node);
-
-        return node.val;
+        ListNode node = this.mp.get(key);
+        this.dll.removeNode(node);
+        this.dll.addNode(node);
+        return node.value;
     }
     
+    /*
+
+    entry exists
+        update value and move it to end
+    entry doesn't exist
+        add to end
+    if entries go beyond the cap, remove the first node
+    */
     public void put(int key, int value) {
         ListNode node;
-        if(this.hm.containsKey(key)) {
-            node = this.hm.get(key);
-            this.dll.remove(node);
-            node.val = value;
+        if(this.mp.containsKey(key)) {
+            node = this.mp.get(key);
+            this.dll.removeNode(node);
+            node.value = value;
         } else {
             node = new ListNode(key, value);
         }
-        this.dll.addLast(node);
-        this.hm.put(key, node);
+        this.dll.addNode(node);
+        this.mp.put(key, node);
 
-        if(this.hm.size() > cap) {
-            this.hm.remove(this.dll.head.key);
-            this.dll.head = this.dll.head.next;
-            if(this.dll.head != null) this.dll.head.prev = null;
-
+        if(this.mp.size() > this.cap) {
+            this.mp.remove(this.dll.head.key);
+            this.dll.removeNode(this.dll.head);
         }
     }
 }
