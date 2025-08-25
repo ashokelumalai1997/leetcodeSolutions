dc5d0ba3ecbc047a531815bc4d3719e2f5eb308f
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-08-25 16:27:15 +0530

    Time: 59 ms (34.92%), Space: 46.1 MB (94.64%) - LeetHub

---
+++ 1905-design-authentication-manager/1905-design-authentication-manager.java
@@ -0,0 +1,40 @@
+class AuthenticationManager {
+
+    Map<String, Integer> mp;
+    int ttl;
+
+    public AuthenticationManager(int timeToLive) {
+        this.ttl = timeToLive;
+        this.mp = new HashMap<>();
+    }
+    
+    public void generate(String tokenId, int currentTime) {
+        this.mp.put(tokenId, currentTime);
+    }
+    
+    public void renew(String tokenId, int currentTime) {
+        // this.mp.remove(tokenId);
+        if(this.mp.containsKey(tokenId) && this.mp.get(tokenId) + ttl > currentTime) {
+            this.mp.put(tokenId, currentTime);
+        }
+        
+    }
+    
+    public int countUnexpiredTokens(int currentTime) {
+        int count = 0;
+        for(Map.Entry<String, Integer> entry : this.mp.entrySet()) {
+            if(entry.getValue() + ttl > currentTime) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * AuthenticationManager obj = new AuthenticationManager(timeToLive);
+ * obj.generate(tokenId,currentTime);
+ * obj.renew(tokenId,currentTime);
+ * int param_3 = obj.countUnexpiredTokens(currentTime);
+ */
