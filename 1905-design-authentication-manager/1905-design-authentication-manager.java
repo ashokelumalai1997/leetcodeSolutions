265e1e88eca2e57e9a192e9893d92df073195324
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2025-08-25 16:40:03 +0530

    Time: 28 ms (90.87%), Space: 46.3 MB (76.39%) - LeetHub

--- 1905-design-authentication-manager/1905-design-authentication-manager.java
+++ 1905-design-authentication-manager/1905-design-authentication-manager.java
@@ -1,15 +1,27 @@
 class AuthenticationManager {
 
+    static class QEntry {
+        String tokenId;
+        Integer time;
+        public QEntry(String tokenId, int time) {
+            this.tokenId = tokenId;
+            this.time = time;
+        }
+    }
+
     Map<String, Integer> mp;
+    ArrayDeque<QEntry> ad;
     int ttl;
 
     public AuthenticationManager(int timeToLive) {
         this.ttl = timeToLive;
         this.mp = new HashMap<>();
+        this.ad = new ArrayDeque<>();
     }
     
     public void generate(String tokenId, int currentTime) {
         this.mp.put(tokenId, currentTime);
+        this.ad.addLast(new QEntry(tokenId, currentTime));
     }
     
     public void renew(String tokenId, int currentTime) {
@@ -18,17 +30,21 @@ class AuthenticationManager {
             return;
         }
         this.mp.put(tokenId, currentTime);
+        this.ad.addLast(new QEntry(tokenId, currentTime));
         
     }
     
     public int countUnexpiredTokens(int currentTime) {
-        int count = 0;
-        for(Map.Entry<String, Integer> entry : this.mp.entrySet()) {
-            if(entry.getValue() + ttl > currentTime) {
-                count++;
+        // int count = 0;
+        int size = ad.size();
+        while(!this.ad.isEmpty() && this.ad.peekFirst().time + ttl <= currentTime) {
+            String token = this.ad.peekFirst().tokenId;
+            if(this.mp.containsKey(token) && this.mp.get(token) + ttl <= currentTime) {
+                this.mp.remove(this.ad.peekFirst().tokenId);
             }
+            this.ad.removeFirst();
         }
-        return count;
+        return mp.size();
     }
 }
 
