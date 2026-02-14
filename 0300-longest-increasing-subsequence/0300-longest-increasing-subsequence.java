9437073ee9e789a0cfd02e0d1dfd38dad4aac6da
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2026-02-14 13:25:58 +0530

    Time: 377 ms (12.3%), Space: 237.2 MB (17.38%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,28 @@
-
 class Solution {
-    private int LIS(int[][] dp, int ind, int lastTakenInd, int[] nums) {
-        if(ind == nums.length) return 0;
-
-
-        if(dp[ind][lastTakenInd+1] != -1) return dp[ind][lastTakenInd+1];
-        int taken = 0;
-        int notTaken = 0;
-        if(lastTakenInd == -1 || nums[lastTakenInd] < nums[ind]) {
-            taken = 1 + LIS(dp, ind+1, ind, nums);
-        }
-
-        notTaken = LIS(dp, ind+1, lastTakenInd, nums);
-
-        return dp[ind][lastTakenInd+1] = Math.max(taken, notTaken);
-    }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
+
         int[][] dp = new int[n][n+1];
 
         for(int i = 0; i < n; i++) {
             Arrays.fill(dp[i], -1);
         }
-        return LIS(dp, 0, -1, nums);
+        return LIS(0, -1, nums, dp);
+    }
+
+    public int LIS(int current, int lastTakenInd, int[] nums, int[][] dp) {
+        if(current == nums.length) {
+            return 0;
+        }
+
+        if(dp[current][lastTakenInd+1] != -1) return dp[current][lastTakenInd+1];
+
+        int taken = 0;
+        if(lastTakenInd == -1 || nums[lastTakenInd] < nums[current]) {
+            taken = 1 + LIS(current + 1, current, nums, dp);
+        }
+        int notTaken = LIS(current+1, lastTakenInd, nums, dp);
+
+        return dp[current][lastTakenInd+1] = Math.max(taken, notTaken);
     }
 }
