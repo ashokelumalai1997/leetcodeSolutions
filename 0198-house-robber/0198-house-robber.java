b9cb323933193e0427544a483a4dd6e9dec5d6af
Ashok Elumalai <31373343+ashokelumalai1997@users.noreply.github.com>
2026-02-16 13:25:21 +0530

    Time: 0 ms (100%), Space: 43 MB (35.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,26 +1,17 @@
 class Solution {
-	public int rob(int[] nums) {
-		int n = nums.length;
-		// int[] dp = new int[n];
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
 
-		// dp[0] = nums[0];
+        if(n == 1) return nums[0];
 
-		if(n == 1) return nums[0];
+        dp[n-1] = nums[n-1];
+        dp[n-2] = Math.max(dp[n-1], nums[n-2]);
 
-		int secondLast = nums[0];
+        for(int i = n-3; i >= 0; i--) {
+            dp[i] = Math.max(nums[i] + dp[i+2], dp[i+1]);
+        }
 
-		int last = Math.max(nums[0], nums[1]);
-
-		
-		int current = last;
-
-
-		for(int i = 2; i < n; i++) {
-			current = Math.max(nums[i] + secondLast, last);
-            secondLast = last;
-            last = current;
-}
-
-return current;
-}
+        return dp[0];
+    }
 }
